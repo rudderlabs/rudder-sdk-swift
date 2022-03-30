c969994cda9f9726402530e3becc05dfa951a563
Pallab Maiti <pallab@rudderstack.com>
2022-03-30 22:02:25 +0530

    Public API exposed from RSServerConfig to fetch destination config by Codable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,3 +8,7 @@ All notable changes to this project will be documented in this file.
 ## Version - 1.0.0-beta.2 - 2022-03-23
 ### Fix
 - Podspec homepage updated.
+
+## Version - 1.0.0-beta.3 - 2022-03-30
+### Added
+- Public API exposed from RSServerConfig to fetch destination config by Codable.
--- Sources/Classes/Domain/Models/Attributes/RSServerConfig.swift
+++ Sources/Classes/Domain/Models/Attributes/RSServerConfig.swift
@@ -138,4 +138,19 @@ extension RSServerConfig {
         }
         return nil
     }
+    
+    private func getConfig<T: Codable>(forKey key: String) -> T? {
+        var result: T? = nil
+        if let destination = getDestination(by: key) {
+            guard let config = destination.config?.dictionaryValue else { return nil }
+            if let jsonData = try? JSONSerialization.data(withJSONObject: config) {
+                result = try? JSONDecoder().decode(T.self, from: jsonData)
+            }
+        }
+        return result
+    }
+    
+    public func getConfig<T: Codable>(forPlugin plugin: RSDestinationPlugin) -> T? {
+        return getConfig(forKey: plugin.key)
+    }
 }
