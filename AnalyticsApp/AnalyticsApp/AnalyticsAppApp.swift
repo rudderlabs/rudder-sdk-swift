a01f52f20aaae17307ea1a6e4fd23cb66af4c684
Satheesh Kannan <satheesh@rudderstack.com>
2024-08-14 00:54:13 +0530

    feat: add Analytics framework and AnalyticsApp iOS project

---
+++ AnalyticsApp/AnalyticsApp/AnalyticsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsAppApp.swift
+//  AnalyticsApp
+//
+//  Created by Satheesh Kannan on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
