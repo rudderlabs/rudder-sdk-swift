43eb08f10e934e33586c414cf3fd6821f7c56d4f
Satheesh Kannan <satheesh@rudderstack.com>
2024-08-19 16:52:43 +0530

    chore: update teat app code to initialize the SDK

--- AnalyticsApp/AnalyticsApp/AnalyticsAppApp.swift
+++ AnalyticsApp/AnalyticsApp/AnalyticsAppApp.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import Analytics
 
 @main
 struct AnalyticsAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        AnalyticsManager.shared.initializeAnalyticsSDK()
+        return true
+    }
+}
+
+class AnalyticsManager {
+    static let shared = AnalyticsManager()
+    
+    var analytics: Analytics?
+   
+    private init() {}
+    
+    func initializeAnalyticsSDK() {
+        let config = Configuration(writeKey: "", dataPlaneUrl: "")
+        self.analytics = Analytics(configuration: config)
     }
 }
--- AnalyticsApp/AnalyticsApp/ContentView.swift
+++ AnalyticsApp/AnalyticsApp/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Track Event")
+                .onTapGesture {}
         }
         .padding()
     }
