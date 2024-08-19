be49528c00e4e207804d7057c122d77f692e0c70
Satheesh Kannan <satheesh@rudderstack.com>
2024-08-19 16:50:08 +0530

    chore: update the AnalyticsClient class

--- Analytics/Analytics/AnalyticsClient.swift
+++ Analytics/Analytics/AnalyticsClient.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+@objcMembers
+public class Analytics {
+    public var configuration: Configuration
+    
+    public init(configuration: Configuration) {
+        self.configuration = configuration
+    }
+}
+
+public struct Constants {
+    public static let logTag = "Rudder-Analytics"
+    public static let defaultLogLevel = LogLevel.none
+    
+    private init() {}
+}
