c02408b573f65e26f865b3770a493d8942588f5e
Satheesh Kannan <satheesh@rudderstack.com>
2024-09-14 00:32:32 +0530

    chore: update the message event class confirm to codable

--- Analytics/Analytics/Events/MessageEvent.swift
+++ Analytics/Analytics/Events/MessageEvent.swift
@@ -9,11 +9,26 @@ import Foundation
 
 // MARK: - EventType
 @objc
-public enum EventType: Int {
+public enum EventType: Int, Codable, CaseIterable {
     case track, screen, alias, identify, group
     
     public var title: String {
-        return String(describing: self).capitalized
+        return switch(self) {
+        case .track:
+            "Track"
+        case .screen:
+            "Screen"
+        case .alias:
+            "Alias"
+        case .identify:
+            "Identify"
+        case .group:
+            "Group"
+        }
+    }
+    
+    public static func from(title: String) -> EventType? {
+        return EventType.allCases.first { $0.title == title }
     }
 }
 
@@ -22,7 +37,7 @@ public enum EventType: Int {
  This is base class for all events.
  */
 @objcMembers
-public class MessageEvent {
+public class MessageEvent: NSObject, Codable {
     public var type: EventType
     public var messageId: String
     public var originalTimeStamp: String
@@ -31,6 +46,34 @@ public class MessageEvent {
         self.type = type
         self.messageId = messageId
         self.originalTimeStamp = originalTimeStamp
+    }
+    
+    // Required init for decoding
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let typeTitle = try container.decode(String.self, forKey: .type)
+        guard let type = EventType.from(title: typeTitle) else {
+            throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Invalid type value")
+        }
+        self.type = type
+        self.messageId = try container.decode(String.self, forKey: .messageId)
+        self.originalTimeStamp = try container.decode(String.self, forKey: .originalTimeStamp)
+    }
+    
+    // Encoding method
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type.title, forKey: .type)
+        try container.encode(messageId, forKey: .messageId)
+        try container.encode(originalTimeStamp, forKey: .originalTimeStamp)
+    }
+    
+    // Define coding keys for encoding and decoding
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case messageId
+        case originalTimeStamp
     }
 }
 
@@ -49,5 +92,34 @@ public class TrackEvent: MessageEvent {
         self.options = options
         
         super.init(type: type, messageId: messageId, originalTimeStamp: originalTimeStamp)
+    }
+    
+    // Required init for decoding
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.event = try container.decode(String.self, forKey: .event)
+        self.properties = try container.decodeDictionary(forKey: .properties)
+        self.options = try container.decodeDictionary(forKey: .options)
+        
+        // Decode the superclass properties
+        try super.init(from: decoder)
+    }
+    
+    // Encoding method
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(event, forKey: .event)
+        try container.encode(properties, forKey: .properties)
+        try container.encode(options, forKey: .options)
+        
+        // Encode the superclass properties
+        try super.encode(to: encoder)
+    }
+    
+    // Define coding keys for encoding and decoding
+    private enum CodingKeys: String, CodingKey {
+        case event
+        case properties
+        case options
     }
 }
