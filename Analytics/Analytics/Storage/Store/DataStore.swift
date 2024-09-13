a2f2ea20bd1c966f499310c9d590db27a15bbfb7
Satheesh Kannan <satheesh@rudderstack.com>
2024-09-14 01:44:02 +0530

    chore: combine protocol functions

--- Analytics/Analytics/Storage/Store/DataStore.swift
+++ Analytics/Analytics/Storage/Store/DataStore.swift
@@ -8,19 +8,8 @@
 import Foundation
 
 public protocol DataStore {
-    func retain<T: Codable>(value: T?, key: String)
-    func retain<T: Codable>(value: T?)
-    func retrieve<T: Codable>(key: String) -> T?
-    func retrieve<T: Codable>(filePath: String) -> T?
-    func remove(key: String)
-    func remove(filePath: String)
+    func retain<T: Codable>(value: T?, reference: String)
+    func retrieve<T: Codable>(reference: String) -> T?
+    func remove(reference: String)
 }
 
-extension DataStore {
-    func retain<T: Codable>(value: T?, key: String) {}
-    func retain<T: Codable>(value: T?) {}
-    func remove(key: String) {}
-    func remove(filePath: String) {}
-    func retrieve<T: Codable>(key: String) -> T? { return nil }
-    func retrieve<T: Codable>(filePath: String) -> T? { return nil }
-}
