d7ccceca48136a29c9c6fc5aaee892cc14d363c7
Satheesh Kannan <satheesh@rudderstack.com>
2024-09-18 14:22:45 +0530

    feat: add mock data provider class for test cases

--- Analytics/AnalyticsTests/MockProvider.swift
+++ Analytics/AnalyticsTests/MockProvider.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Analytics
+
+final class MockProvider {
+    private init() {}
+    
+    private static let _mockWriteKey = "MoCk_WrItEkEy"
+    
+    static let clientWithDiskStorage: AnalyticsClient = {
+        let storage = BasicStorage(writeKey: _mockWriteKey, storageMode: .disk)
+        let configuration = Configuration(writeKey: _mockWriteKey, dataPlaneUrl: "https://www.mock-url.com/", storage: storage)
+        
+        return AnalyticsClient(configuration: configuration)
+    }()
+    
+    static let clientWithMemoryStorage: AnalyticsClient = {
+        let storage = BasicStorage(writeKey: _mockWriteKey, storageMode: .memory)
+        let configuration = Configuration(writeKey: _mockWriteKey, dataPlaneUrl: "https://www.mock-url.com/", storage: storage)
+        
+        return AnalyticsClient(configuration: configuration)
+    }()
+    
+    static let simpleTrackEvent: TrackEvent = {
+        let event = TrackEvent(event: "Track_Event", properties: ["Property_1": .string("Value1")], options: ["Property_1": .string("Value1")])
+        return event
+    }()
+}
