372b92580b844b848430614305cb6bdebbbe40c2
Satheesh Kannan <satheesh@rudderstack.com>
2024-10-06 00:22:13 +0530

    chore: add screen, group, flush event struct defenition

--- Analytics/Analytics/Events/Message.swift
+++ Analytics/Analytics/Events/Message.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // MARK: - EventType
 public enum EventType: String, CaseIterable, Codable {
-    case track, screen, alias, identify, group
+    case track, screen, group, flush
     
     public var label: String {
         return rawValue.capitalized
@@ -26,11 +26,25 @@ public protocol Message: Codable {
     var originalTimeStamp: String { get set }
 }
 
+extension Message {
+    public var messageId: String {
+        get {
+            return .randomUUIDString
+        }
+        set{}
+    }
+    
+    public var originalTimeStamp: String {
+        get {
+            return .currentTimeStamp
+        }
+        set{}
+    }
+}
+
 // MARK: - TrackEvent
 public struct TrackEvent: Message {
     public var type: EventType
-    public var messageId: String
-    public var originalTimeStamp: String
     
     public var event: String
     public var properties: CodableDictionary?
@@ -38,10 +52,49 @@ public struct TrackEvent: Message {
     
     public init(event: String, properties: CodableDictionary?, options: CodableDictionary?) {
         self.type = .track
-        self.messageId = .randomUUIDString
-        self.originalTimeStamp = .currentTimeStamp
         self.event = event
         self.properties = properties
         self.options = options
+    }
+}
+
+public struct ScreenEvent: Message {
+    public var type: EventType
+    
+    public var screenName: String
+    public var properties: CodableDictionary?
+    public var options: CodableDictionary?
+    
+    public init(screenName: String, properties: CodableDictionary?, options: CodableDictionary?) {
+        self.type = .screen
+        self.screenName = screenName
+        self.properties = properties
+        self.options = options
+    }
+}
+
+public struct GroupEvent: Message {
+    public var type: EventType
+    
+    public var groupId: String
+    public var traits: CodableDictionary?
+    public var options: CodableDictionary?
+    
+    init(groupId: String, traits: CodableDictionary? = nil, options: CodableDictionary? = nil) {
+        self.type = .group
+        self.groupId = groupId
+        self.traits = traits
+        self.options = options
+    }
+}
+
+public struct FlushEvent: Message {
+    public var type: EventType
+    
+    public var messageName: String
+    
+    init(messageName: String) {
+        self.type = .flush
+        self.messageName = messageName
     }
 }
