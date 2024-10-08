2db81823e39b08144eda44a28f3a7f9cb0ab79b4
Satheesh Kannan <satheesh@rudderstack.com>
2024-10-08 19:46:35 +0530

    feat: add message manager class to handle the message related operations

--- Analytics/Analytics/Events/MessageManager.swift
+++ Analytics/Analytics/Events/MessageManager.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+
+final class MessageManager {
+    var analytics: AnalyticsClient
+    
+    private var writeChannel: AsyncChannel<Message>
+    private var uploadChannel: AsyncChannel<String>
+    
+    init(analytics: AnalyticsClient) {
+        self.analytics = analytics
+        
+        self.writeChannel = AsyncChannel()
+        self.uploadChannel = AsyncChannel()
+        
+        self.start()
+    }
+    
+    deinit {
+        self.writeChannel.closeChannel()
+        self.uploadChannel.closeChannel()
+    }
+    
+    func put(_ message: Message) {
+        guard message.type != .flush else { self.flush(); return }
+        self.writeChannel.send(message)
+    }
+    
+    func flush() {
+        self.uploadChannel.send(Constants.uploadSignal)
+    }
+    
+    private func start() {
+        Task.detached {
+            await self.writeChannel.consume { message in
+                self.performStorage(message)
+            }
+        }
+        
+        Task.detached {
+            await self.uploadChannel.consume { value in
+                self.startUploading()
+            }
+        }
+    }
+}
+
+extension MessageManager {
+    var storage: Storage {
+        return self.analytics.configuration.storage
+    }
+    
+    func performStorage(_ message: Message) {
+        Task {
+            guard let json = message.toJSONString else { return }
+            self.storage.write(message: json)
+        }
+    }
+}
+
+extension MessageManager {
+    func startUploading() {
+        // TODO: will work....
+    }
+}
