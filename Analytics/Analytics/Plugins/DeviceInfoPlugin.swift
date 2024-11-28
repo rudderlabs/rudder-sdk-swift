4f230b49a95410a3c6ebf5f994514e820d86cf1b
Satheesh Kannan <satheesh@rudderstack.com>
2024-11-28 10:38:17 +0530

    chore: update device info plugin defenition

--- Analytics/Analytics/Plugins/DeviceInfoPlugin.swift
+++ Analytics/Analytics/Plugins/DeviceInfoPlugin.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import UIKit
+
+final class DeviceInfoPlugin: Plugin {
+    
+    var pluginType: PluginType = .preProcess
+    var analytics: AnalyticsClient?
+    var collectDeviceId = false
+    
+    func setup(analytics: AnalyticsClient) {
+        self.analytics = analytics
+        self.collectDeviceId = analytics.configuration.collectDeviceId
+    }
+    
+    func execute(event: any Message) -> (any Message)? {
+        return self.attachDeviceInfo(to: event)
+    }
+}
+
+extension DeviceInfoPlugin {
+    private func attachDeviceInfo(to event: any Message) -> any Message {
+        var message = event
+        var context = message.context ?? [:]
+        
+        context["device"] = AnyCodable(self.preparedDeviceInfo)
+        message.context = context
+        
+        return message
+    }
+    
+    var preparedDeviceInfo: [String: Any] {
+        var deviceInfo = [String: Any]()
+        
+        deviceInfo["id"] = self.collectDeviceId ? UIDevice.current.identifierForVendor?.uuidString : self.analytics?.anonymousId
+        deviceInfo["manufacturer"] = "Apple"
+        deviceInfo["model"] = self.deviceModelIdentifier
+        deviceInfo["name"] = UIDevice.current.name
+        deviceInfo["type"] = UIDevice.current.systemName
+        
+        return deviceInfo
+    }
+    
+    var deviceModelIdentifier: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.compactMap { value -> String? in
+            guard let value = value.value as? Int8, value != 0 else { return nil }
+            return String(UnicodeScalar(UInt8(value)))
+        }.joined()
+        return identifier
+    }
+}
