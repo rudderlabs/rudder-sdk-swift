38b2a65d83b6fee387bfcfba33039a9f8221aa31
Satheesh Kannan <satheesh@rudderstack.com>
2024-12-25 00:02:26 +0530

    chore: add test cases to test the network values

--- Analytics/AnalyticsTests/PluginTests/NetworkInfoPluginTests.swift
+++ Analytics/AnalyticsTests/PluginTests/NetworkInfoPluginTests.swift
@@ -41,4 +41,104 @@ final class NetworkInfoPluginTests: XCTestCase {
             }
         }
     }
+    
+    func test_wifi_cellular_enabled_trackEvent() {
+        given("An simple track event to the network info plugin..") {
+            let plugin = NetworkInfoPlugin()
+            let track = TrackEvent(event: "Track")
+            
+            when("Wifi and Cellular is enabled..") {
+                let mockMonitor = MockNetworkMonitor(status: .satisfied, interfaces: [.wifi, .cellular])
+                let mockUtils = NetworkInfoPluginUtils(monitor: mockMonitor)
+                plugin.networkInfoUtils = mockUtils
+                
+                let executedEvent = plugin.execute(event: track)
+                
+                then("track event should have the network info details..") {
+                    guard let context = executedEvent?.context else { XCTFail("No context found"); return }
+                    XCTAssertNotNil(context["network"])
+                    
+                    guard let codableInfo = context["network"], let networkInfo = codableInfo.value as? [String: Any] else { XCTFail("No network info found"); return }
+                    
+                    XCTAssertEqual(networkInfo["wifi"] as? Bool, true)
+                    XCTAssertEqual(networkInfo["cellular"] as? Bool, true)
+                }
+            }
+        }
+    }
+    
+    func test_wifi_enabled_trackEvent() {
+        given("An simple track event to the network info plugin..") {
+            let plugin = NetworkInfoPlugin()
+            let track = TrackEvent(event: "Track")
+            
+            when("Wifi and Cellular is enabled..") {
+                let mockMonitor = MockNetworkMonitor(status: .satisfied, interfaces: [.wifi])
+                let mockUtils = NetworkInfoPluginUtils(monitor: mockMonitor)
+                plugin.networkInfoUtils = mockUtils
+                
+                let executedEvent = plugin.execute(event: track)
+                
+                then("track event should have the network info details..") {
+                    guard let context = executedEvent?.context else { XCTFail("No context found"); return }
+                    XCTAssertNotNil(context["network"])
+                    
+                    guard let codableInfo = context["network"], let networkInfo = codableInfo.value as? [String: Any] else { XCTFail("No network info found"); return }
+                                        
+                    XCTAssertEqual(networkInfo["wifi"] as? Bool, true)
+                    XCTAssertEqual(networkInfo["cellular"] as? Bool, false)
+                }
+            }
+        }
+    }
+    
+    func test_cellular_enabled_trackEvent() {
+        given("An simple track event to the network info plugin..") {
+            let plugin = NetworkInfoPlugin()
+            let track = TrackEvent(event: "Track")
+            
+            when("Wifi and Cellular is enabled..") {
+                let mockMonitor = MockNetworkMonitor(status: .satisfied, interfaces: [.cellular])
+                let mockUtils = NetworkInfoPluginUtils(monitor: mockMonitor)
+                plugin.networkInfoUtils = mockUtils
+                
+                let executedEvent = plugin.execute(event: track)
+                
+                then("track event should have the network info details..") {
+                    guard let context = executedEvent?.context else { XCTFail("No context found"); return }
+                    XCTAssertNotNil(context["network"])
+                    
+                    guard let codableInfo = context["network"], let networkInfo = codableInfo.value as? [String: Any] else { XCTFail("No network info found"); return }
+                                        
+                    XCTAssertEqual(networkInfo["wifi"] as? Bool, false)
+                    XCTAssertEqual(networkInfo["cellular"] as? Bool, true)
+                }
+            }
+        }
+    }
+    
+    func test_wifi_cellular_not_enabled_trackEvent() {
+        given("An simple track event to the network info plugin..") {
+            let plugin = NetworkInfoPlugin()
+            let track = TrackEvent(event: "Track")
+            
+            when("Wifi and Cellular is enabled..") {
+                let mockMonitor = MockNetworkMonitor(status: .satisfied, interfaces: [])
+                let mockUtils = NetworkInfoPluginUtils(monitor: mockMonitor)
+                plugin.networkInfoUtils = mockUtils
+                
+                let executedEvent = plugin.execute(event: track)
+                
+                then("track event should have the network info details..") {
+                    guard let context = executedEvent?.context else { XCTFail("No context found"); return }
+                    XCTAssertNotNil(context["network"])
+                    
+                    guard let codableInfo = context["network"], let networkInfo = codableInfo.value as? [String: Any] else { XCTFail("No network info found"); return }
+                    
+                    XCTAssertEqual(networkInfo["wifi"] as? Bool, false)
+                    XCTAssertEqual(networkInfo["cellular"] as? Bool, false)
+                }
+            }
+        }
+    }
 }
