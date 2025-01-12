244ff817274d184f0118936a92f50177f17132fe
Satheesh Kannan <satheesh@rudderstack.com>
2025-01-12 20:16:06 +0530

    feat: implement useridentity class
    * feat: add user identity model class
    
    * chore: update user identity class implementation
    
    * chore: update key value store logic and add related extensions, storage keys
    
    * chore: add user identity model test cases
    
    * chore: update mock key value storage logic
    
    * chore: update user identity class and test cases
    
    * chore: fix merge conflict
    
    * chore: update user identity access specifiers and file references
    
    * chore: remove unused variables

---
+++ Analytics/Analytics/StateManagement/UserIdentity.swift
@@ -0,0 +1,37 @@
+//
+//  UserIdentity.swift
+//  Analytics
+//
+//  Created by Satheesh Kannan on 07/01/25.
+//
+
+import Foundation
+
+/**
+ A struct representing the identity of a user.
+
+ - Properties:
+   - `anonymousId`: A unique identifier for the user when they are not logged in. Defaults to an empty string.
+
+ - Methods:
+   - `initializeState(_:)`: Creates and initializes a `UserIdentity` instance by reading stored data from a key-value storage.
+ */
+
+struct UserIdentity {
+    /// A unique identifier for the user when they are not logged in.
+    var anonymousId = ""
+
+    /**
+     Creates and initializes a `UserIdentity` instance by reading data from the provided key-value storage.
+
+     - Parameter storage: An instance of `KeyValueStorage` used to read the stored user identity data.
+     - Returns: A fully initialized `UserIdentity` object with data from the storage or default values if not available.
+     */
+    static func initializeState(_ storage: KeyValueStorage) -> UserIdentity {
+        var identity = UserIdentity()
+
+        identity.anonymousId = storage.read(key: StorageKeys.anonymousId) ?? .randomUUIDString
+
+        return identity
+    }
+}
---
+++ Analytics/AnalyticsTests/StateManagementTests/UserIdentityTests.swift
@@ -0,0 +1,56 @@
+//
+//  UserIdentityTests.swift
+//  AnalyticsTests
+//
+//  Created by Satheesh Kannan on 08/01/25.
+//
+
+import Foundation
+import XCTest
+@testable import Analytics
+
+final class UserIdentityTests: XCTestCase {
+    
+    private var storage: MockKeyValueStorage?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        storage = MockKeyValueStorage()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        storage = nil
+    }
+    
+    func test_initializeState_emptyStorage() {
+        given("Prepare empty storage..") {
+            guard let storage else { XCTFail("Storage is not initialized."); return }
+            
+            when("UserIdentity initialized using empty storage.") {
+                let userIdentity = UserIdentity.initializeState(storage)
+                
+                then("UserIdentity initialized with default values") {
+                    XCTAssertFalse(userIdentity.anonymousId.isEmpty, "Anonymous ID should be generated")
+                }
+            }
+        }
+    }
+    
+    func test_initializeState_notEmptyStorage() {
+        given("Prepare storage with values..") {
+            guard let storage else { XCTFail("Storage is not initialized."); return }
+            
+            let expectedAnonymousId = "test-anonymous-id"
+            storage.write(value: expectedAnonymousId, key: StorageKeys.anonymousId)
+            
+            when("UserIdentity initialized.") {
+                let userIdentity = UserIdentity.initializeState(storage)
+                
+                then("UserIdentity initialized with storage values") {
+                    XCTAssertEqual(userIdentity.anonymousId, expectedAnonymousId, "Anonymous ID should match stored value")
+                }
+            }
+        }
+    }
+}
