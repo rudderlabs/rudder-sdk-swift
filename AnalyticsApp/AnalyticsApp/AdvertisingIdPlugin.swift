c964ec449f3da51f06e62f02f03b4b62bfc2a308
Satheesh Kannan <satheesh@rudderstack.com>
2025-01-20 20:40:35 +0530

    chore: update advertising id plugin

--- AnalyticsApp/AnalyticsApp/AdvertisingIdPlugin.swift
+++ AnalyticsApp/AnalyticsApp/AdvertisingIdPlugin.swift
@@ -5,4 +5,34 @@
 //  Created by Satheesh Kannan on 20/01/25.
 //
 
-import Foundation
+import Analytics
+import AdSupport
+import AppTrackingTransparency
+
+class AdvertisingIdPlugin: Plugin {
+    
+    var pluginType: PluginType = .preProcess
+    var analytics: AnalyticsClient?
+    
+    func setup(analytics: AnalyticsClient) {
+        self.analytics = analytics
+    }
+    
+    func execute(event: any Message) -> (any Message)? {
+        guard let advertisingId = self.advertisementId else { return event }
+        
+        var deviceContent = [String: Any]()
+        if let deviceInfo = event.context?["device"]?.value as? [String: Any] {
+            deviceInfo.forEach { deviceContent[$0.key] = $0.value }
+        }
+        
+        // Add the IDFA to the device context
+        deviceContent["advertisingId"] = advertisingId
+        return event.addToContext(info: ["device": deviceContent])
+    }
+    
+    var advertisementId: String? {
+        guard ATTrackingManager.trackingAuthorizationStatus == .authorized else { return nil }
+        return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    }
+}
