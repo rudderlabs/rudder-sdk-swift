e7ac2d2abf5cf934d6db35b378373ad0f6ab602c
Satheesh Kannan <satheesh@rudderstack.com>
2025-01-20 21:39:31 +0530

    feat: add testing target to the sample app and add advertising id plugin test case file

---
+++ AnalyticsApp/AnalyticsAppTests/AdvertisingIdPluginTests.swift
@@ -0,0 +1,8 @@
+//
+//  AdvertisingIdPluginTests.swift
+//  AnalyticsAppTests
+//
+//  Created by Satheesh Kannan on 20/01/25.
+//
+
+import Foundation
---
+++ AnalyticsApp/AnalyticsAppTests/AnalyticsAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnalyticsAppTests.swift
+//  AnalyticsAppTests
+//
+//  Created by Satheesh Kannan on 20/01/25.
+//
+
+import Testing
+
+struct AnalyticsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
