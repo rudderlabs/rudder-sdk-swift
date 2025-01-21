0f1a0a61b0cbf034fc5b493c6f534a27d1f0de1a
Satheesh Kannan <satheesh@rudderstack.com>
2025-01-21 23:38:52 +0530

    chore: add storage keys and add helping extensions

--- Analytics/Analytics/Helpers/Extensions.swift
+++ Analytics/Analytics/Helpers/Extensions.swift
@@ -29,6 +29,11 @@ extension String {
     var trimmedUrlString: String {
         return self.hasSuffix("/") ? String(self.dropLast()) : self
     }
+    
+    var toDictionary: [String: Any]? {
+        guard let data = self.utf8Data else { return nil }
+        return try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+    }
 }
 
 // MARK: - DateFormatter
@@ -158,6 +163,13 @@ extension URL {
 extension [String: AnyCodable] {
     static func + (lhs: [Key: Value], rhs: [Key: Value]) -> [Key: Value] {
         return lhs.merging(rhs) { (_, new) in new }
+    }
+}
+
+// MARK: - [String: Any]
+extension [String: Any] {
+    var jsonString: String? {
+        return try? JSONSerialization.data(withJSONObject: self, options: []).jsonString
     }
 }
 
