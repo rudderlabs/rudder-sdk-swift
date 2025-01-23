c7f7bebe33187a317cc5e532df376791e3e7afdb
Satheesh Kannan <satheesh@rudderstack.com>
2025-01-24 00:51:27 +0530

    chore: update set userid traits and external ids action reduce method

--- Analytics/Analytics/StateManagement/SetUserIdTraitsAndExternalIdsAction.swift
+++ Analytics/Analytics/StateManagement/SetUserIdTraitsAndExternalIdsAction.swift
@@ -13,18 +13,31 @@ struct SetUserIdTraitsAndExternalIdsAction: StateAction {
     private let userId: String
     private let traits: [String: Any]
     private let externalIds: [ExternalId]
+    private let analytics: AnalyticsClient
     
-    init(userId: String, traits: [String : Any], externalIds: [ExternalId]) {
+    init(userId: String, traits: [String : Any], externalIds: [ExternalId], analytics: AnalyticsClient) {
         self.userId = userId
         self.traits = traits
         self.externalIds = externalIds
+        self.analytics = analytics
     }
     
     func reduce(currentState: UserIdentity) -> UserIdentity {
         var newState = currentState
         newState.userId = userId
-        newState.traits = traits
-        newState.externalIds = externalIds
+        
+        let isUserIdChanged = currentState.userId != userId
+        
+        if isUserIdChanged {
+            currentState.resetUserIdTraitsAndExternalIds(self.analytics.configuration.storage)
+        
+            newState.traits = traits
+            newState.externalIds = externalIds
+        } else {
+            newState.traits = currentState.traits + self.traits
+            newState.externalIds = currentState.externalIds.mergeWithHigherPriorityTo(self.externalIds)
+        }
+        
         return newState
     }
 }
