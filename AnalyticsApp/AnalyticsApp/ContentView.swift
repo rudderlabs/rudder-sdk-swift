f115987c9b9aaf46249c70fce033f7f8e0aa7bcb
Satheesh Kannan <satheesh@rudderstack.com>
2025-01-24 17:28:58 +0530

    chore: update sample app to trigger full identify event

--- AnalyticsApp/AnalyticsApp/ContentView.swift
+++ AnalyticsApp/AnalyticsApp/ContentView.swift
@@ -15,7 +15,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             CustomButton(title: "Identify") {
-                AnalyticsManager.shared.identify(userId: "12345")
+                let options = RudderOptions()
+                    .addIntegration("Amplitude", isEnabled: false)
+                    .addCustomContext("identify_key1", key: "identify_value1")
+                    .addExternalId(ExternalId(type: "idCardNumber", id: "12791"))
+                
+                
+                AnalyticsManager.shared.identify(userId: "12345", traits: ["IdentifyTraits_key1": "IdentifyTraits_value1"], options: options)
             }
             
             HStack {
