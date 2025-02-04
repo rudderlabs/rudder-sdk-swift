27c8a6b556b9f097ff6027736efed4478c8e00a9
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-04 23:56:40 +0530

    chore: add reset user identity action test cases

---
+++ Analytics/AnalyticsTests/StateManagementTests/ResetUserIdentityActionTests.swift
@@ -0,0 +1,56 @@
+//
+//  ResetUserIdentityActionTests.swift
+//  AnalyticsTests
+//
+//  Created by Satheesh Kannan on 04/02/25.
+//
+
+import XCTest
+@testable import Analytics
+
+final class ResetUserIdentityActionTests: XCTestCase {
+    
+    func test_resetAction_withoutAnonymousId() {
+        given("Prepare a test user identity reference with a reset action...") {
+            let anonymousId = "testAnonymousId"
+            let userId = "testUserId"
+            let traits = ["testKey": "testValue"]
+            let externalId = [ExternalId(type: "test_type", id: "test_id")]
+            
+            let state = createState(initialState: UserIdentity(anonymousId: anonymousId, userId: userId, traits: traits, externalIds: externalId))
+            let action = ResetUserIdentityAction(clearAnonymousId: false)
+            when("Update state with action..") {
+                state.dispatch(action: action)
+                
+                then("UserIdentity values are reset except anonymousId..") {
+                    XCTAssertEqual(state.state.value.anonymousId, anonymousId)
+                    XCTAssertTrue(state.state.value.userId.isEmpty)
+                    XCTAssertTrue(state.state.value.traits.isEmpty)
+                    XCTAssertTrue(state.state.value.externalIds.isEmpty)
+                }
+            }
+        }
+    }
+    
+    func test_resetAction_withAnonymousId() {
+        given("Prepare a test user identity reference with a reset action...") {
+            let anonymousId = "testAnonymousId"
+            let userId = "testUserId"
+            let traits = ["testKey": "testValue"]
+            let externalId = [ExternalId(type: "test_type", id: "test_id")]
+            
+            let state = createState(initialState: UserIdentity(anonymousId: anonymousId, userId: userId, traits: traits, externalIds: externalId))
+            let action = ResetUserIdentityAction(clearAnonymousId: true)
+            when("Update state with action..") {
+                state.dispatch(action: action)
+                
+                then("UserIdentity all values are reseted..") {
+                    XCTAssertNotEqual(state.state.value.anonymousId, anonymousId)
+                    XCTAssertTrue(state.state.value.userId.isEmpty)
+                    XCTAssertTrue(state.state.value.traits.isEmpty)
+                    XCTAssertTrue(state.state.value.externalIds.isEmpty)
+                }
+            }
+        }
+    }
+}
