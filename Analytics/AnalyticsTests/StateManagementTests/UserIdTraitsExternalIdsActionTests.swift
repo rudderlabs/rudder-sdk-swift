52a914cd873bf18bdb44a1954bea99221daddc1e
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-04 19:05:30 +0530

    chore: update test cases as per the pr comments

--- Analytics/AnalyticsTests/StateManagementTests/UserIdTraitsExternalIdsActionTests.swift
+++ Analytics/AnalyticsTests/StateManagementTests/UserIdTraitsExternalIdsActionTests.swift
@@ -22,7 +22,7 @@ final class UserIdTraitsExternalIdsActionTests: XCTestCase {
         storage = nil
     }
     
-    func test_updateAction_singleUserId() {
+    func test_updateAction() {
         given("Prepare test UserIdentity state with initial set of values and action with expected set of values...") {
             guard let storage else { XCTFail("Storage is not initialized."); return }
             
@@ -32,59 +32,43 @@ final class UserIdTraitsExternalIdsActionTests: XCTestCase {
             
             let expectedUserId = "test-user-id"
             let expectedTraits = ["traits_key": "traits_value", "traits_key2": "sk@example.com"]
-            let expectedExternalIds = [ExternalId(type: "sample_type", id: "sample_id")]
+            let expectedExternalIds = [ExternalId(type: "sample_type1", id: "sample_id1"), ExternalId(type: "sample_type2", id: "sample_id2")]
             
-            let state = createState(initialState: UserIdentity(userId: initialUserId, traits: initialTraits, externalIds: initialExternalIds))
+            let processingState = createState(initialState: UserIdentity(userId: initialUserId, traits: initialTraits, externalIds: initialExternalIds))
             let action = SetUserIdTraitsAndExternalIdsAction(userId: expectedUserId, traits: expectedTraits, externalIds: expectedExternalIds, storage: storage)
             
             when("Update initial state of UserIdentity with expected values..") {
-                state.dispatch(action: action)
+                processingState.dispatch(action: action)
                 
                 then("UserIdentity updated with expected values") {
-                    XCTAssertEqual(state.state.value.userId, expectedUserId, "The userId should be updated to the new value.")
-                    XCTAssertEqual(state.state.value.traits["traits_key2"] as? String, expectedTraits["traits_key2"], "The traits should be updated to the new value.")
-                    XCTAssertEqual(state.state.value.externalIds[0].id, expectedExternalIds[0].id, "The externalIds should be updated to the new value.")
+                    XCTAssertEqual(processingState.state.value.userId, expectedUserId, "The userId should be updated to the new value.")
+                    XCTAssertEqual(processingState.state.value.traits as? [String : String], expectedTraits, "The traits should be updated to the new value.")
+                    XCTAssertEqual(processingState.state.value.externalIds.count, expectedExternalIds.count, "The externalIds should be updated to the new value.")
                 }
             }
         }
     }
     
     func test_updateAction_appendValues() {
-        given("Prepare test UserIdentity state with first set of values and action with second set of values...") {
+        given("Prepare test UserIdentity state with first set of values..") {
             guard let storage else { XCTFail("Storage is not initialized."); return }
             
             let firstUserId = "first_test_user_id"
             let firstTraits = ["first_traits_key": "first_traits_value"]
             let firstExternalIds = [ExternalId(type: "first_sample_type", id: "first_sample_id")]
             
-            let secondUserId = "test-user-id"
             let secondTraits = ["traits_key": "traits_value", "traits_key2": "sk@example.com"]
             let secondExternalIds = [ExternalId(type: "sample_type", id: "sample_id")]
             
-            let state = createState(initialState: UserIdentity(userId: firstUserId, traits: firstTraits, externalIds: firstExternalIds))
-            let action = SetUserIdTraitsAndExternalIdsAction(userId: secondUserId, traits: secondTraits, externalIds: secondExternalIds, storage: storage)
+            let processingState = createState(initialState: UserIdentity(userId: firstUserId, traits: firstTraits, externalIds: firstExternalIds))
             
-            when("Update initial state of UserIdentity with second set of values..") {
-                state.dispatch(action: action)
+            when("Update current state of UserIdentity with added set of values and same userId...") {
+                let newAction = SetUserIdTraitsAndExternalIdsAction(userId: firstUserId, traits: secondTraits, externalIds: secondExternalIds, storage: storage)
+                processingState.dispatch(action: newAction)
                 
-                then("UserIdentity updated with expected values") {
-                    XCTAssertEqual(state.state.value.userId, secondUserId, "The userId should be updated to the new value.")
-                    XCTAssertEqual(state.state.value.traits["traits_key2"] as? String, secondTraits["traits_key2"], "The traits should be updated to the new value.")
-                    XCTAssertEqual(state.state.value.externalIds[0].id, secondExternalIds[0].id, "The externalIds should be updated to the new value.")
-                }
-                
-                when("append the values with same userId..") {
-                    let newAction = SetUserIdTraitsAndExternalIdsAction(userId: secondUserId, traits: firstTraits, externalIds: firstExternalIds, storage: storage)
-                    
-                    when("Update current state of UserIdentity with added set of values..") {
-                        state.dispatch(action: newAction)
-                        
-                        then("UserIdentity updated with expected values") {
-                            XCTAssertEqual(state.state.value.userId, secondUserId, "The userId should be updated to the new value.")
-                            XCTAssertEqual(state.state.value.traits["first_traits_key"] as? String, firstTraits["first_traits_key"], "The traits should be updated to the new value.")
-                            XCTAssertEqual(state.state.value.externalIds[1].id, firstExternalIds[0].id, "The externalIds should be updated to the new value.")
-                        }
-                    }
+                then("UserIdentity updated with appended values...") {
+                    XCTAssertEqual(processingState.state.value.traits as? [String : String], firstTraits + secondTraits, "The traits should be updated with the added values.")
+                    XCTAssertEqual(processingState.state.value.externalIds.count, (firstExternalIds + secondExternalIds).count, "The externalIds should be updated with the added values.")
                 }
             }
         }
