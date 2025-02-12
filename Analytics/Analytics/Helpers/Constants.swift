b680310d06aa77b6922fa8ab96a98f5583781697
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-12 12:41:44 +0530

    feat: add revamped constants file

---
+++ Analytics/Analytics/Helpers/Constants.swift
@@ -0,0 +1,92 @@
+//
+//  Constants.swift
+//  Analytics
+//
+//  Created by Satheesh Kannan on 11/02/25.
+//
+
+import Foundation
+
+public struct RSConstants {
+    private init() {} // Prevent instantiation
+}
+
+// MARK: - Log
+extension RSConstants {
+    public struct Log {
+        public static let tag: String = "Rudder-Analytics"
+        public static let defaultLevel: LogLevel = .none
+        private init() {}
+    }
+}
+
+// MARK: - Storage
+extension RSConstants {
+    public struct Storage {
+        public struct Keys {
+            public static let anonymousId = "anonymous_id"
+            public static let userId = "user_id"
+            public static let traits = "traits"
+            public static let externalIds = "external_ids"
+            public static let sourceConfig = "source_config"
+            private init() {}
+        }
+
+        static let fileIndex = "rudderstack.message.file.index."
+        static let memoryIndex = "rudderstack.message.memory.index."
+        static let fileNameSeparator: String = "-"
+        static let fileType = "tmp"
+        
+        private init() {}
+    }
+}
+
+// MARK: - Flush Policies
+extension RSConstants {
+    public struct Flush {
+
+        public struct EventCount {
+            public static let `default` = 30
+            public static let min = 1
+            public static let max = 100
+            
+            private init() {}
+        }
+
+        public struct Interval {
+            public static let `default`: Double = 10_000 // 10 seconds
+            public static let min: Double = 1
+        }
+
+        private init() {}
+    }
+
+}
+
+// MARK: - Payload
+extension RSConstants {
+    public struct Payload {
+        static let maxSize: Int64 = 32 * 1024 // 32 KB
+        static let maxBatchSize: Int64 = 500 * 1024 // 500 KB
+        static let batchPrefix = "{\"batch\":["
+        static let batchSentAtSuffix = "],\"sentAt\":\""
+        static let batchSuffix = "\"}"
+        static let sentAtPlaceholder = "{{_RSA_DEF_SENT_AT_TS_}}"
+        static let channel = "mobile"
+        static let integration = ["All": true]
+        private init() {}
+    }
+}
+
+// MARK: - Defaults
+extension RSConstants {
+    public struct DefaultConfig {
+        public static let storageMode: StorageMode = .disk
+        public static let controlPlaneUrl: String = "https://api.rudderlabs.com"
+        public static let gzipEnabled: Bool = true
+        public static let flushPolicies: [FlushPolicy] = [StartupFlushPolicy(), FrequencyFlushPolicy(), CountFlushPolicy()]
+        public static let willCollectDeviceId: Bool = true
+        static let queryParams = ["p": "ios", "v": "\(RSVersion)"]
+        static let uploadSignal = "#!upload!#"
+    }
+}
