dc5f06b14d6ab99631d2c1ab36137f6c8b25ee55
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-18 15:51:59 +0530

    chore: remove bluetooth related logics from network info plugin
    * chore: remove bluetooth related logics from network info plugin
    
    * feat: add permission manager to handle the all the user permissions
    
    * chore: remove legacy code for idfa permission
    
    * chore: update permission manager to handle the user permissions
    
    * chore: activate permission manager to ask user permission before initiating rudder sdk
    
    * feat: add bluetooth info plugin
    
    * chore: add the bluetooth info external plugin to analytics
    
    * chore: update bluetooth info plugin implementation
    
    * feat: add bluetooth info plugin test cases
    
    * chore: update sample project structure

--- AnalyticsApp/AnalyticsApp/ContentView.swift
+++ AnalyticsApp/AnalyticsApp/ContentView.swift
@@ -7,8 +7,6 @@
 
 import SwiftUI
 import Analytics
-import AdSupport
-import AppTrackingTransparency
 
 struct ContentView: View {
     
@@ -97,28 +95,6 @@ struct ContentView: View {
                 CustomButton(title: "Reset (AnonymousId)") {
                     AnalyticsManager.shared.reset(true)
                 }
-            }
-        }
-        .ignoresSafeArea(.all)
-        .onAppear {
-            self.confirmTrackingPermission()
-        }
-    }
-}
-
-extension ContentView {
-    func confirmTrackingPermission() {
-        print("Requesting Tracking Permission...")
-        Task.detached {
-            let status = await ATTrackingManager.requestTrackingAuthorization()
-            print("Tracking Status: \(status.rawValue)")
-            
-            if status == .authorized {
-                let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
-                print("IDFA: \(idfa)")
-                print("Tracking authorized.")
-            } else {
-                print("Tracking not authorized.")
             }
         }
     }
---
+++ AnalyticsApp/AnalyticsAppTests/BluetoothInfoPluginTests.swift
@@ -0,0 +1,65 @@
+//
+//  BluetoothInfoPluginTests.swift
+//  AnalyticsAppTests
+//
+//  Created by Satheesh Kannan on 17/02/25.
+//
+
+import Testing
+import Analytics
+@testable import AnalyticsApp
+
+struct BluetoothInfoPluginTests {
+    
+    @Test
+    func test_bluetoothAvailability_whenAuthorized() {
+        given("Prepare the environment when authorization is authorized...") {
+            let config = Configuration(writeKey: "sample-write-key", dataPlaneUrl: "https://data-plane.analytics.com")
+            let analytics = AnalyticsClient(configuration: config)
+            
+            let bluetoothInfoPlugin = BluetoothInfoPlugin()
+            analytics.addPlugin(bluetoothInfoPlugin)
+            
+            let event = MockEvent()
+            
+            bluetoothInfoPlugin.bluetoothAuthorizationStatus = { .allowedAlways }
+            
+            when("execute the plugin using mock event..") {
+                let result = bluetoothInfoPlugin.execute(event: event)
+                
+                then("check the result if bluetooth status added or not..") {
+                    #expect(result != nil)
+                    
+                    guard let networkContext = result?.context?["network"]?.value as? [String: Any], let isBluetoothAvailable = networkContext["bluetooth"] as? Bool else {
+                        #expect(1 == 0, "bluetooth status not found.."); return
+                    }
+                    #expect(isBluetoothAvailable)
+                }
+            }
+        }
+    }
+    
+    @Test
+    func test_bluetoothAvailability_whenDenied() {
+        given("Prepare the environment when authorization is denied..") {
+            let config = Configuration(writeKey: "sample-write-key", dataPlaneUrl: "https://data-plane.analytics.com")
+            let analytics = AnalyticsClient(configuration: config)
+            
+            let bluetoothInfoPlugin = BluetoothInfoPlugin()
+            analytics.addPlugin(bluetoothInfoPlugin)
+            
+            let event = MockEvent()
+            
+            bluetoothInfoPlugin.bluetoothAuthorizationStatus = { .denied }
+            
+            when("execute the plugin using mock event..") {
+                let result = bluetoothInfoPlugin.execute(event: event)
+                
+                then("check the result if bluetooth status added or not..") {
+                    #expect(result != nil)
+                    #expect(result?.context?["network"] == nil, "bluetooth status not found..")
+                }
+            }
+        }
+    }
+}
