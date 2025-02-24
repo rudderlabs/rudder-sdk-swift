c71a0cc00329c9e235d239a1e06e81189113c13f
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-25 00:45:45 +0530

    chore: update session manager implementation

--- Analytics/Analytics/SessionManagement/SessionManager.swift
+++ Analytics/Analytics/SessionManagement/SessionManager.swift
@@ -7,6 +7,53 @@
 
 import Foundation
 
-class SessionManager {
+final class SessionManager {
     
+    private var storage: Storage
+    private var sessionState: StateImpl<SessionState>
+    
+    private var sessionInstance: SessionState {
+        return self.sessionState.state.value
+    }
+    
+    init (storage: Storage) {
+        self.storage = storage
+        self.sessionState = createState(initialState: SessionState.initState(storage))
+    }
+    
+    func startSession(sessionId: Int64, isManualSession: Bool) {
+        
+    }
+    
+    func endSession() {
+        
+    }
+    
+    var sessionId: Int64? {
+        return self.sessionInstance.sessionId == SessionConstants.defaultSessionId ? nil : self.sessionInstance.sessionId
+    }
+}
+
+extension SessionManager {
+    
+    static var generatedSessionId: Int64 {
+        return Int64(Date().timeIntervalSince1970)
+    }
+    
+    private func withSessionDispatcher(_ block: @escaping () async -> Void) {
+        Task { await block() }
+    }
+}
+
+
+struct SessionConstants {
+    static let minSessionIdLength = 10
+    static let defaultSessionId: Int64 = 0
+    static let defaultLastActivityTime: Int64 = 0
+    static let defaultIsManualSession: Bool = false
+    static let defaultIsSessionStarted: Bool = false
+    
+    private init() {
+        /* Prevent instantiation (no-op) */
+    }
 }
