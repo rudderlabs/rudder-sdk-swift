7b9ea970eb0c1e13b1675ecd001ef8b78be13d12
Satheesh Kannan <satheesh@rudderstack.com>
2025-02-25 15:02:20 +0530

    chore: update session manager implementation on start session

--- Analytics/Analytics/SessionManagement/SessionManager.swift
+++ Analytics/Analytics/SessionManagement/SessionManager.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+// MARK: - SessionType
+
+enum SessionType {
+    case manual
+    case automatic
+}
+
+// MARK: - SessionManager
+
 final class SessionManager {
     
     private var storage: Storage
@@ -16,17 +25,21 @@ final class SessionManager {
         return self.sessionState.state.value
     }
     
-    init (storage: Storage) {
+    init(storage: Storage) {
         self.storage = storage
         self.sessionState = createState(initialState: SessionState.initState(storage))
     }
     
-    func startSession(sessionId: UInt64, isManualSession: Bool) {
-        
+    func startSession(id: UInt64, type: SessionType, shouldUpdateType: Bool = true) {
+        self.updateSesstionStart(isSessionStrat: true)
+        if shouldUpdateType {
+            self.updateSessionType(type: type)
+        }
+        self.updateSessionId(id: id)
     }
     
     func endSession() {
-        
+        /* temp implementation (no-op) */
     }
     
     var sessionId: UInt64? {
@@ -34,24 +47,42 @@ final class SessionManager {
     }
 }
 
+// MARK: - Helpers
+
 extension SessionManager {
     
     static var generatedSessionId: UInt64 {
         return UInt64(Date().timeIntervalSince1970)
     }
     
-    private func withSessionDispatcher(_ block: @escaping () async -> Void) {
-        Task { await block() }
+    private func updateSessionId(id: UInt64) {
+        self.sessionState.dispatch(action: UpdateSessionId(sessionId: id))
+        self.sessionState.state.value.storeSessionType(type: .automatic, storage: self.storage)
+    }
+    
+    private func updateSesstionStart(isSessionStrat: Bool) {
+        guard self.sessionInstance.isSessionStart != isSessionStrat else { return }
+        
+        self.sessionState.dispatch(action: UpdateIsSessionStart(isSessionStart: isSessionStrat))
+        self.sessionState.state.value.storeIsSessionStart(isSessionStart: isSessionStrat, storage: self.storage)
+    }
+    
+    private func updateSessionType(type: SessionType) {
+        guard self.sessionInstance.sessionType != type else { return }
+        
+        self.sessionState.dispatch(action: UpdateSessionType(sessionType: type))
+        self.sessionState.state.value.storeSessionType(type: type, storage: self.storage)
     }
 }
 
+// MARK: - SessionConstants
 
 struct SessionConstants {
     static let minSessionIdLength = 10
     static let defaultSessionId: UInt64 = 0
     static let defaultLastActivityTime: UInt64 = 0
-    static let defaultIsManualSession: Bool = false
-    static let defaultIsSessionStarted: Bool = false
+    static let defaultSessionType: SessionType = .automatic
+    static let defaultIsSessionStart: Bool = false
     
     private init() {
         /* Prevent instantiation (no-op) */
