46b67239b76ef6030f9cd57d6845c55c66d2d37f
Satheesh Kannan <satheesh@rudderstack.com>
2025-03-10 22:08:59 +0530

    chore: update lifecycle management utils

--- Analytics/Analytics/LifecycleManagement/LifecycleManagementUtils.swift
+++ Analytics/Analytics/LifecycleManagement/LifecycleManagementUtils.swift
@@ -5,4 +5,45 @@
 //  Created by Satheesh Kannan on 10/03/25.
 //
 
-import Foundation
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
+enum AppLifecycleEvent: CaseIterable {
+    case background
+    case terminate
+    case foreground
+    
+    var notificationName: Notification.Name {
+#if os(macOS)
+        switch self {
+        case .background: return NSApplication.didResignActiveNotification
+        case .terminate: return NSApplication.willTerminateNotification
+        case .foreground: return NSApplication.didBecomeActiveNotification
+        }
+#else
+        switch self {
+        case .background: return UIApplication.didEnterBackgroundNotification
+        case .terminate: return UIApplication.willTerminateNotification
+        case .foreground: return UIApplication.willEnterForegroundNotification
+        }
+#endif
+    }
+}
+
+protocol LifecycleEventObserver: AnyObject {
+    func onBackground()
+    func onForeground()
+    func onTerminate()
+}
+
+// Wrapper to hold weak references to observers
+class WeakObserver {
+    weak var observer: LifecycleEventObserver?
+    
+    init(_ observer: LifecycleEventObserver) {
+        self.observer = observer
+    }
+}
