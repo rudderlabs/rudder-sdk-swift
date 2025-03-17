9925dedf39d61a9e7ca74334b3481e634e64eca0
Satheesh Kannan <satheesh@rudderstack.com>
2025-03-17 12:46:34 +0530

    chore: update lifecycle tracking plugin implementation

--- Analytics/Analytics/Plugins/LifecycleTrackingPlugin.swift
+++ Analytics/Analytics/Plugins/LifecycleTrackingPlugin.swift
@@ -15,14 +15,14 @@ final class LifecycleTrackingPlugin: Plugin {
     var pluginType: PluginType = .manual
     
     var analytics: AnalyticsClient?
-    private var appVersion: AppVersion?
+    var appVersion: AppVersion?
     
     @Synchronized private var isFirstLaunch = true
     
     func setup(analytics: AnalyticsClient) {
         self.analytics = analytics
         
-        self.appVersion = self.getAppVersion()
+        self.appVersion = self.prepareAppVersion()
         self.updateAppVersion()
         
         if analytics.configuration.trackApplicationLifecycleEvents {
@@ -36,6 +36,8 @@ final class LifecycleTrackingPlugin: Plugin {
     }
 }
 
+// MARK: - LifecycleEventListener
+
 extension LifecycleTrackingPlugin: LifecycleEventListener {
     func onBecomeActive() {
         var properties: [String: Any] = [:]
@@ -44,25 +46,27 @@ extension LifecycleTrackingPlugin: LifecycleEventListener {
         }
         properties["from_background"] = !isFirstLaunch
         isFirstLaunch = false
-        self.analytics?.track(name: "Application Opened", properties: properties)
+        self.analytics?.track(name: LifecycleEvent.applicationOpened.rawValue, properties: properties)
     }
     
     func onBackground() {
-        self.analytics?.track(name: "Application Backgrounded")
+        self.analytics?.track(name: LifecycleEvent.applicationBackgrounded.rawValue)
     }
 }
+
+// MARK: - Installation Events
 
 extension LifecycleTrackingPlugin {
     func trackAppInstallationEvents() {
         guard let appVersion else { return }
         if appVersion.previousBuild == -1 {
-            self.analytics?.track(name: "Application Installed", properties: [
+            self.analytics?.track(name: LifecycleEvent.applicationInstalled.rawValue, properties: [
                 "version": appVersion.currentVersionName ?? "",
                 "build": appVersion.currentBuild
             ])
         } else {
             guard appVersion.currentBuild != appVersion.previousBuild else { return }
-            self.analytics?.track(name: "Application Updated", properties: [
+            self.analytics?.track(name: LifecycleEvent.applicationUpdated.rawValue, properties: [
                 "version": appVersion.currentVersionName ?? "",
                 "build": appVersion.currentBuild,
                 "previous_version": appVersion.previousVersionName ?? "",
@@ -72,8 +76,10 @@ extension LifecycleTrackingPlugin {
     }
 }
 
+// MARK: - Version Updaters
+
 extension LifecycleTrackingPlugin {
-    private func getAppVersion() -> AppVersion {
+    private func prepareAppVersion() -> AppVersion {
         let bundle = Bundle.main
         let currentVersionName = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
         let currentBuild = Int(bundle.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? "0") ?? 0
@@ -94,9 +100,20 @@ extension LifecycleTrackingPlugin {
     }
 }
 
+// MARK: - AppVersion
+
 struct AppVersion {
     var currentVersionName: String?
     var currentBuild: Int
     var previousVersionName: String?
     var previousBuild: Int
+}
+
+// MARK: - LifecycleEvent
+
+enum LifecycleEvent: String {
+    case applicationInstalled = "Application Installed"
+    case applicationUpdated = "Application Updated"
+    case applicationOpened = "Application Opened"
+    case applicationBackgrounded = "Application Backgrounded"
 }
