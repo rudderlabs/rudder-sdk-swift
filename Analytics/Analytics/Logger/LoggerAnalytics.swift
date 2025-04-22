3e5bb370ec4242e9011046f9e23c7f21c0a53f02
Satheesh Kannan <satheesh@rudderstack.com>
2025-04-22 22:36:33 +0530

    chore: make logger analytics public

--- Analytics/Analytics/Logger/LoggerAnalytics.swift
+++ Analytics/Analytics/Logger/LoggerAnalytics.swift
@@ -11,25 +11,25 @@ import Foundation
 
 /**
  A centralized logger used for logging analytics-related events based on a set log level.
-*/
+ */
 
-final class LoggerAnalytics {
+public final class LoggerAnalytics {
     
     // MARK: - Private Properties
-
+    
     private init() {
         // Default implementation is a no-op singleton
     }
-
+    
     private static let shared = LoggerAnalytics()
-
+    
     private var logger: Logger?
     private var logLevel: LogLevel = Constants.Log.defaultLevel
-
+    
     /**
      Sets up the logger and log level for analytics.
      
-    - Parameters:
+     - Parameters:
         - logger: A custom logger conforming to the `Logger` protocol.
         - logLevel: The minimum level of logs to be shown.
      */
@@ -38,30 +38,57 @@ final class LoggerAnalytics {
         shared.logger = logger
         shared.logLevel = logLevel
     }
-
+    
     // MARK: - Log Methods
-
-    static func verbose(log: String) {
+    
+    /**
+     Logs a verbose message if the current log level allows it.
+     
+     - Parameter log: The verbose message to be logged.
+     */
+    public static func verbose(log: String) {
         guard shared.logLevel.rawValue >= LogLevel.verbose.rawValue else { return }
         shared.logger?.verbose(log: log)
     }
-
-    static func debug(log: String) {
+    
+    /**
+     Logs a debug message if the current log level allows it.
+     
+     - Parameter log: The debug message to be logged.
+     */
+    public static func debug(log: String) {
         guard shared.logLevel.rawValue >= LogLevel.debug.rawValue else { return }
         shared.logger?.debug(log: log)
     }
-
-    static func info(log: String) {
+    
+    /**
+     Logs an info message if the current log level allows it.
+     
+     - Parameter log: The info message to be logged.
+     */
+    public static func info(log: String) {
         guard shared.logLevel.rawValue >= LogLevel.info.rawValue else { return }
         shared.logger?.info(log: log)
     }
-
-    static func warn(log: String) {
+    
+    /**
+     Logs a warning message if the current log level allows it.
+     
+     - Parameter log: The warning message to be logged.
+     */
+    public static func warn(log: String) {
         guard shared.logLevel.rawValue >= LogLevel.warn.rawValue else { return }
         shared.logger?.warn(log: log)
     }
-
-    static func error(log: String, error: Error? = nil) {
+    
+    /**
+     Logs an error message if the current log level allows it.
+     
+     - Parameters:
+     - log: The error message to be logged.
+     - error: An optional `Error` instance to be included with the log.
+     */
+    public static func error(log: String, error: Error? = nil) {
         guard shared.logLevel.rawValue >= LogLevel.error.rawValue else { return }
         shared.logger?.error(log: log, error: error)
     }
