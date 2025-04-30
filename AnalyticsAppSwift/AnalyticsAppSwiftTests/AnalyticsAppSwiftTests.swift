7d9610b6c15ac5bb101e0be82a58e9fb9a6cadb9
Satheesh Kannan <satheesh@rudderstack.com>
2025-04-30 23:53:23 +0530

    faet: add unit test case module

---
+++ AnalyticsAppSwift/AnalyticsAppSwiftTests/AnalyticsAppSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnalyticsAppSwiftTests.swift
+//  AnalyticsAppSwiftTests
+//
+//  Created by Satheesh Kannan on 30/04/25.
+//
+
+import Testing
+
+struct AnalyticsAppSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
