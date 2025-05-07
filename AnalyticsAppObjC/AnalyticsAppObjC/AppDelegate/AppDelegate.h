0e60c85c9f44e815ff08023f7d11fbbf8d147823
Satheesh Kannan <satheesh@rudderstack.com>
2025-05-07 22:50:46 +0530

    feat: add objective c based sample app

---
+++ AnalyticsAppObjC/AnalyticsAppObjC/AppDelegate/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AnalyticsAppObjC
+//
+//  Created by Satheesh Kannan on 07/05/25.
+//
+
+#import <UIKit/UIKit.h>
+#import <Analytics/Analytics.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AnalyticsAppObjC/AnalyticsAppObjC/ViewControllers/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AnalyticsAppObjC
+//
+//  Created by Satheesh Kannan on 07/05/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
