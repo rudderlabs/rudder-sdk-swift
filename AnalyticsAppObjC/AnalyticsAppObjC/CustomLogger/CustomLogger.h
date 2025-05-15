5f2844c5b14b2cd6be1a35b46d587ea774cd2665
Satheesh Kannan <satheesh@rudderstack.com>
2025-05-15 16:22:15 +0530

    feat: add objc custom logger implementation

---
+++ AnalyticsAppObjC/AnalyticsAppObjC/CustomLogger/CustomLogger.h
@@ -0,0 +1,17 @@
+//
+//  CustomLogger.h
+//  AnalyticsAppObjC
+//
+//  Created by Satheesh Kannan on 15/05/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <Analytics/Analytics-Swift.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomLogger : NSObject<Logger>
+
+@end
+
+NS_ASSUME_NONNULL_END
