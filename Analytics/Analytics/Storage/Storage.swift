19c4036db603030ee768f6ab2443336d266c15e5
Satheesh Kannan <satheesh@rudderstack.com>
2025-05-21 15:37:21 +0530

    refactor: restore codable protocol usage in key value storage

--- Analytics/Analytics/Storage/Storage.swift
+++ Analytics/Analytics/Storage/Storage.swift
@@ -12,10 +12,11 @@ import Foundation
  A protocol that defines a simple key-value storage mechanism.
 
  This protocol provides methods for storing, retrieving, and removing data in a storage system using string-based keys.
+ It supports encoding and decoding of data types that conform to the `Codable` protocol.
 
  - Methods:
-   - `write(value: Any?, key: String)`: Stores a value in the storage for a given key.
-   - `read(key: String) -> Any?`: Retrieves a value associated with the given key.
+   - `write<T: Codable>(value: T, key: String)`: Stores a value in the storage for a given key.
+   - `read<T: Codable>(key: String) -> T?`: Retrieves a value associated with the given key.
    - `remove(key: String)`: Removes a value associated with the given key.
  */
 public protocol KeyValueStorage {
@@ -23,19 +24,18 @@ public protocol KeyValueStorage {
      Stores a value for a given key.
      
      - Parameters:
-        - value: The value to store.
+        - value: The value to store. Must conform to the `Codable` protocol.
         - key: The key associated with the value.
      */
-    func write(value: Any?, key: String)
+    func write<T: Codable>(value: T, key: String)
     
     /**
      Retrieves a value associated with the given key.
-
-     - Parameters:
-        - key: The key for which to retrieve the value.
-        - Returns: The stored value, or `nil` if no value exists for the key.
+     
+     - Parameter key: The key for which to retrieve the value.
+     - Returns: The decoded value, or `nil` if no value exists for the key or if decoding fails.
      */
-    func read(key: String) -> Any?
+    func read<T: Codable>(key: String) -> T?
     
     /**
      Removes a value associated with the given key.
