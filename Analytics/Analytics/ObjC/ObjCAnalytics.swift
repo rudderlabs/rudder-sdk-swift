f232ffafa5a32e4e21f167664ca0a52a4a817326
Satheesh Kannan <satheesh@rudderstack.com>
2025-05-23 16:08:21 +0530

    chore: expose session related public api to objc

--- Analytics/Analytics/ObjC/ObjCAnalytics.swift
+++ Analytics/Analytics/ObjC/ObjCAnalytics.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// MARK: - ObjCAnalytics
+
 @objc(RSAnalytics)
 public final class ObjCAnalytics: NSObject {
     
@@ -16,5 +18,33 @@ public final class ObjCAnalytics: NSObject {
     public init(configuration: ObjCConfiguration) {
         self.analytics = AnalyticsClient(configuration: configuration.configuration)
     }
+}
+
+// MARK: - Session
+extension ObjCAnalytics {
     
+    @objc
+    public func startSession() {
+        self.analytics.startSession()
+    }
+    
+    @objc
+    public func startSession(sessionId: NSNumber) {
+        if sessionId.int64Value < 0 {
+            LoggerAnalytics.error(log: "Negative session IDs are invalid.")
+            return
+        }
+        self.analytics.startSession(sessionId: sessionId.uint64Value)
+    }
+    
+    @objc
+    public func endSession() {
+        self.analytics.endSession()
+    }
+    
+    @objc
+    public var sessionId: NSNumber? {
+        guard let sessionId = self.analytics.sessionId else { return nil }
+        return NSNumber(value: sessionId)
+    }
 }
