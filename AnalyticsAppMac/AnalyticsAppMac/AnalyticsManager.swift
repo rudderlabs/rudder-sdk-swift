85a025f14f8e567f8624292fa90618e36a2dfebf
Satheesh Kannan <satheesh@rudderstack.com>
2025-05-30 16:41:45 +0530

    chore: integrate analytics package into mac application

---
+++ AnalyticsAppMac/AnalyticsAppMac/AnalyticsManager.swift
@@ -0,0 +1,35 @@
+//
+//  AnalyticsManager.swift
+//  AnalyticsAppMac
+//
+//  Created by Satheesh Kannan on 30/05/25.
+//
+
+import Foundation
+import Analytics
+
+// MARK: - AnalyticsManager
+class AnalyticsManager {
+    
+    static let shared = AnalyticsManager()
+    private init() {}
+    
+    private var analytics: AnalyticsClient?
+    
+    func initializeAnalyticsSDK() {
+        let config = Configuration(writeKey: "sample-write-key", dataPlaneUrl: "https://data-plane.analytics.com", logLevel: .verbose)
+        self.analytics = AnalyticsClient(configuration: config)
+    }
+    
+    func track(name: String, properties: RudderProperties? = nil, options: RudderOption? = nil) {
+        self.analytics?.track(name: name, properties: properties, options: options)
+    }
+    
+    func screen(name: String, category: String? = nil, properties: RudderProperties? = nil, options: RudderOption? = nil) {
+        self.analytics?.screen(name: name, category: category, properties: properties, options: options)
+    }
+    
+    func flush() {
+        self.analytics?.flush()
+    }
+}
--- AnalyticsAppMac/AnalyticsAppMac/ContentView.swift
+++ AnalyticsAppMac/AnalyticsAppMac/ContentView.swift
@@ -6,14 +6,27 @@
 //
 
 import SwiftUI
+import Analytics
 
+// MARK: - ContentView
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            CustomButton(title: "Track Event") {
+                let option = RudderOption(integrations: ["Amplitude": true, "CleverTap": false], customContext: ["SK1": ["Key1": "Value1"], "SK2": ["value1", "value2"], "SK3": "Value3", "SK4": 1234, "SK5": 5678.9, "SK6": true], externalIds: [ExternalId(type: "idCardNumber", id: "12791")])
+                
+                AnalyticsManager.shared.track(name: "Track at \(Date())", properties: ["key": "value"], options: option)
+            }
+            
+            CustomButton(title: "Multiple Track Event") {
+                for i in 1...100 {
+                    AnalyticsManager.shared.track(name: "Track Event: \(i)")
+                }
+            }
+            
+            CustomButton(title: "Flush") {
+                AnalyticsManager.shared.flush()
+            }
         }
         .padding()
     }
@@ -21,4 +34,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+// MARK: - CustomButton
+struct CustomButton: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .fontWeight(.semibold)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.accentColor.opacity(0.1))
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.accentColor, lineWidth: 1)
+                )
+                .foregroundColor(Color.accentColor)
+        }
+        .buttonStyle(PlainButtonStyle())
+        .padding(6)
+    }
 }
