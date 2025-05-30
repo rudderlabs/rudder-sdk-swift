1e90b21d934f37c3aa53404e25fa2f5d95f9ec73
Satheesh Kannan <satheesh@rudderstack.com>
2025-05-30 14:15:00 +0530

    chore: integrate analytics framework into mac sample app

--- AnalyticsAppMac/AnalyticsAppMac/AnalyticsAppMacApp.swift
+++ AnalyticsAppMac/AnalyticsAppMac/AnalyticsAppMacApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Analytics
 
 // MARK: - AnalyticsAppMacApp
 @main
@@ -24,7 +25,29 @@ struct AnalyticsAppMacApp: App {
 // MARK: - AppDelegate
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    var analytics: AnalyticsClient?
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
         
+    }
+}
+
+// MARK: - Analytics Helper
+
+extension AppDelegate {
+    
+    static var `default`: AppDelegate { NSApplication.shared.delegate as! AppDelegate }
+    
+    func initializeAnalyticsSDK() {
+        let config = Configuration(writeKey: "sample-write-key", dataPlaneUrl: "https://data-plane.analytics.com", logLevel: .verbose)
+        self.analytics = AnalyticsClient(configuration: config)
+    }
+    
+    func track(name: String, properties: RudderProperties? = nil, options: RudderOption? = nil) {
+        self.analytics?.track(name: name, properties: properties, options: options)
+    }
+    
+    func screen(name: String, category: String? = nil, properties: RudderProperties? = nil, options: RudderOption? = nil) {
+        self.analytics?.screen(name: name, category: category, properties: properties, options: options)
     }
 }
