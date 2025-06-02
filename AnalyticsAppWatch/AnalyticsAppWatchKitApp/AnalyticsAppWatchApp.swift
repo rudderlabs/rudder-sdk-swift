f8447f1a9aa5329b67ed42402e210c0b852a8023
Satheesh Kannan <satheesh@rudderstack.com>
2025-06-02 22:33:48 +0530

    feat: add watchOS sample app to test compatibility

---
+++ AnalyticsAppWatch/AnalyticsAppWatchKitApp/AnalyticsAppWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsAppWatchApp.swift
+//  AnalyticsAppWatchKitApp
+//
+//  Created by Satheesh Kannan on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsAppWatchKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalyticsAppWatch/AnalyticsAppWatchKitApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnalyticsAppWatchKitApp
+//
+//  Created by Satheesh Kannan on 02/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
