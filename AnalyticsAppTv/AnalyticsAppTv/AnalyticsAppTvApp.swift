f2beb68d4f884a308c5dd51a9efa8a9daffad10d
Satheesh Kannan <satheesh@rudderstack.com>
2025-06-04 14:12:33 +0530

    feat: add tvOS sample app to test

---
+++ AnalyticsAppTv/AnalyticsAppTv/AnalyticsAppTvApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsAppTvApp.swift
+//  AnalyticsAppTv
+//
+//  Created by Satheesh Kannan on 04/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsAppTvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalyticsAppTv/AnalyticsAppTv/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnalyticsAppTv
+//
+//  Created by Satheesh Kannan on 04/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
