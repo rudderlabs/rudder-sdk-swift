ee1f2a28dcdb694d1136c6afa561215d2a8ff7ab
Satheesh Kannan <satheesh@rudderstack.com>
2025-06-12 13:35:16 +0530

    feat: add objc set push token plugin class

---
+++ AnalyticsAppObjC/AnalyticsAppObjC/SetPushTokenPlugin/SetPushTokenPlugin.h
@@ -0,0 +1,16 @@
+//
+//  SetPushTokenPlugin.h
+//  AnalyticsAppObjC
+//
+//  Created by Satheesh Kannan on 12/06/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SetPushTokenPlugin : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AnalyticsAppObjC/AnalyticsAppObjC/SetPushTokenPlugin/SetPushTokenPlugin.m
@@ -0,0 +1,12 @@
+//
+//  SetPushTokenPlugin.m
+//  AnalyticsAppObjC
+//
+//  Created by Satheesh Kannan on 12/06/25.
+//
+
+#import "SetPushTokenPlugin.h"
+
+@implementation SetPushTokenPlugin
+
+@end
