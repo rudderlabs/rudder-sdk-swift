690a006e9955698d9ad2948c473b73dc6bc95932
Vishal Gupta <vguptakota1998@gmail.com>
2025-06-21 17:08:24 +0530

    refactor: rename flushCount internal param to flushAt in CountFlushPolicy

--- Sources/Policies/CountFlushPolicy.swift
+++ Sources/Policies/CountFlushPolicy.swift
@@ -32,7 +32,7 @@ import Foundation
  */
 public final class CountFlushPolicy: FlushPolicy {
     /// The maximum number of events before a flush is triggered.
-    private(set) var flushCount: Int
+    private(set) var flushAt: Int
 
     /// The current count of events, updated in a thread-safe manner.
     @Synchronized private var eventCount: Int = 0
@@ -44,7 +44,7 @@ public final class CountFlushPolicy: FlushPolicy {
      - Note: The flush count is clamped between `Constants.flushEventCount.min` and `Constants.flushEventCount.max`.
      */
     public init(flushAt: Int = Constants.flushEventCount.default) {
-        self.flushCount = min(Constants.flushEventCount.max, max(flushAt, Constants.flushEventCount.min))
+        self.flushAt = min(Constants.flushEventCount.max, max(flushAt, Constants.flushEventCount.min))
     }
 
     /**
@@ -60,7 +60,7 @@ public final class CountFlushPolicy: FlushPolicy {
      - Returns: A `Bool` indicating whether the flush count has been reached or exceeded.
      */
     public func shouldFlush() -> Bool {
-        return self.eventCount >= self.flushCount
+        return self.eventCount >= self.flushAt
     }
 
     /**
