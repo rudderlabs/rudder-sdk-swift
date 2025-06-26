51d040820c28a3b04054b849833a18c28a1f2b6c
Satheesh Kannan <satheesh@rudderstack.com>
2025-06-26 20:26:54 +0530

    chore: add documentation for CustomOptionPlugin class

--- Examples/ObjCExample/ObjCExample/CustomPlugins/CustomOptionPlugin/CustomOptionPlugin.m
+++ Examples/ObjCExample/ObjCExample/CustomPlugins/CustomOptionPlugin/CustomOptionPlugin.m
@@ -19,6 +19,13 @@
 @implementation CustomOptionPlugin
 @synthesize pluginType;
 
+/**
+ Initializes the plugin with the specified RSSOption.
+ 
+ @param option The RSSOption containing custom context, external IDs, and integration
+               configurations to be applied to all events.
+ @return An initialized instance of CustomOptionPlugin.
+ */
 - (instancetype)initWithOption:(RSSOption *)option
 {
     self = [super init];
@@ -28,10 +35,24 @@
     return self;
 }
 
+/**
+ Returns the plugin type for this plugin.
+ 
+ @return RSSPluginTypeOnProcess, indicating this plugin runs during event processing.
+ */
 - (RSSPluginType)pluginType {
     return RSSPluginTypeOnProcess;
 }
 
+/**
+ Intercepts an event and applies custom options to it.
+ 
+ This method enhances the event by applying custom context, external IDs, and
+ integration configurations from the stored RSSOption.
+ 
+ @param event The event to be processed.
+ @return The modified event with custom options applied.
+ */
 - (RSSEvent * _Nullable)intercept:(RSSEvent * _Nonnull)event {
     
     [self addCustomContext:event];
@@ -41,12 +62,25 @@
     return event;
 }
 
+/**
+ Merges custom context data from the option into the event's context.
+ 
+ @param event The event whose context will be updated with custom data.
+ */
 - (void)addCustomContext:(RSSEvent *)event {
     NSMutableDictionary *contextDict = [NSMutableDictionary dictionaryWithDictionary: (event.context ?: @{})];
     [contextDict addEntriesFromDictionary: (self.option.customContext ?: @{})];
     event.context = contextDict;
 }
 
+/**
+ Adds external IDs from the option to the event's context.
+ 
+ External IDs are appended to any existing external IDs in the event context.
+ Each external ID is converted to a dictionary with 'id' and 'type' keys.
+ 
+ @param event The event whose context will be updated with external IDs.
+ */
 - (void)addExternalIds:(RSSEvent *)event {
     NSMutableDictionary *contextDict = [NSMutableDictionary dictionaryWithDictionary: (event.context ?: @{})];
     NSMutableArray *externalIdArray = [NSMutableArray arrayWithArray: (contextDict[@"externalId"] ?: @[])];
@@ -60,16 +94,31 @@
     event.context = contextDict;
 }
 
+/**
+ Merges integration configurations from the option into the event's integrations.
+ 
+ @param event The event whose integrations will be updated with custom configurations.
+ */
 - (void)addIntegrations:(RSSEvent *)event {
     NSMutableDictionary *integrationsDict = [NSMutableDictionary dictionaryWithDictionary: (event.integrations ?: @{})];
     [integrationsDict addEntriesFromDictionary: (self.option.integrations ?: @{})];
     event.integrations = integrationsDict;
 }
 
+/**
+ Sets up the plugin with the provided analytics client.
+ 
+ @param analytics The analytics client instance to be used by the plugin.
+ */
 - (void)setup:(RSSAnalytics * _Nonnull)analytics {
     self.client = analytics;
 }
 
+/**
+ Performs teardown operations for the plugin.
+ 
+ Called when the plugin is removed from the analytics client.
+ */
 - (void)teardown { }
 
 @end
