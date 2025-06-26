7532abf1d2523f4a919971e1734cb0e595954d06
Satheesh Kannan <satheesh@rudderstack.com>
2025-06-26 23:47:50 +0530

    chore: add missing class-level documentation for SwiftUI example app classes

--- Examples/SwiftUIExample/SwiftUIExample/CustomPlugins/OptionPlugin.swift
+++ Examples/SwiftUIExample/SwiftUIExample/CustomPlugins/OptionPlugin.swift
@@ -10,26 +10,40 @@ import RudderStackAnalytics
 
 // MARK: - OptionPlugin
 /**
- This class is a processing plugin that updates option values of any event.
+ This plugin is to add custom options to all analytics events. It can add custom context data, external IDs, and integration settings to every event that passes through the pipeline.
+ 
+ ## Usage:
+ ```swift
+ // Create options with custom data
+ let options = RudderOption(integrations: ["Amplitude": true, "Mixpanel": false], customContext: ["appVersion": "1.2.3", "userTier": "premium"], externalIds: [ExternalId(type: "userId", id: "user123")])
+  
+ // Create and add the plugin
+ let optionPlugin = OptionPlugin(option: options)
+ analytics.add(plugin: optionPlugin)
+ ```
  */
-
 final class OptionPlugin: Plugin {
     var pluginType: PluginType = .onProcess
     var analytics: Analytics?
     var option: RudderOption?
     
+    /** Creates a new OptionPlugin with the specified options */
     init(option: RudderOption) {
         self.option = option
     }
     
+    /** Called by RudderStack when the plugin is added to the analytics instance */
     func setup(analytics: Analytics) {
         self.analytics = analytics
     }
     
+    /** Intercepts every analytics event and adds the configured options */
     func intercept(event: any Event) -> (any Event)? {
+        // If no options are configured, return the event unchanged
         guard self.option != nil else { return event }
         var updatedEvent = event
         
+        // Add all the configured options to the event
         self.addCustomContext(&updatedEvent)
         self.addExternalIds(&updatedEvent)
         self.addIntegrations(&updatedEvent)
@@ -42,12 +56,22 @@ final class OptionPlugin: Plugin {
 
 extension OptionPlugin {
     
+    /**
+     Adds custom context data to the event
+     
+     - Parameter event: The event to modify (passed by reference)
+     */
     func addCustomContext(_ event: inout any Event) {
         var contextDict = event.context?.rawDictionary ?? [:]
         self.option?.customContext?.forEach { contextDict[$0.key] = $0.value }
         event.context = contextDict.codableWrapped
     }
     
+    /**
+     Adds external IDs to the event for linking with other systems
+      
+     - Parameter event: The event to modify (passed by reference)
+     */
     func addExternalIds( _ event: inout any Event) {
         var contextDict = event.context?.rawDictionary ?? [:]
         if let ids = self.option?.externalIds?.map({ ["id": $0.id, "type": $0.type] }) {
@@ -60,6 +84,11 @@ extension OptionPlugin {
         event.context = contextDict.codableWrapped
     }
     
+    /**
+     Adds integration settings to control event delivery
+            
+     - Parameter event: The event to modify (passed by reference)
+     */
     func addIntegrations(_ event: inout any Event) {
         var integrationsDict = event.integrations?.rawDictionary ?? [:]
         self.option?.integrations?.forEach { integrationsDict[$0.key] = $0.value }
