b93f5cd4cc2e2be1d89b06641ffdbd42806238bb
Satheesh Kannan <satheesh@rudderstack.com>
2025-06-30 18:29:36 +0530

    feat: add license info in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// License: Elastic License 2.0 (ELv2) – see LICENSE file for details
 
 import PackageDescription
 
