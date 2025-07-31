20f8c581304938919cf9d657957943156208c36c
Satheesh Kannan <satheesh@rudderstack.com>
2025-07-31 13:29:12 +0530

    chore: update event filtering plugin implementation

--- Examples/SwiftUIExample/SwiftUIExample/CustomPlugins/EventFilteringPlugin.swift
+++ Examples/SwiftUIExample/SwiftUIExample/CustomPlugins/EventFilteringPlugin.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RudderStackAnalytics
+
+final class EventFilteringPlugin: Plugin {
+    var pluginType: PluginType = .onProcess
+    var analytics: Analytics?
+    
+    private var eventsToFilter = [String]()
+    
+    func setup(analytics: Analytics) {
+        self.analytics = analytics
+        self.eventsToFilter = ["Application Opened", "Application Backgrounded"]
+    }
+    
+    func intercept(event: any Event) -> (any Event)? {
+        if let trackEvent = event as? TrackEvent, eventsToFilter.contains(trackEvent.event) {
+            LoggerAnalytics.verbose(log: "EventFilteringPlugin: Event \"\(trackEvent.event)\" is filtered out.")
+            return nil
+        }
+        return event
+    }
+    
+    func teardown() {
+        eventsToFilter.removeAll()
+    }
+}
