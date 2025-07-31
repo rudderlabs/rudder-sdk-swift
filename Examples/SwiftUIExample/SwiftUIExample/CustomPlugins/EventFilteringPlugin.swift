4bc18dbabac8d94a7c400e2b68c0641e2457d7ad
Satheesh Kannan <satheesh@rudderstack.com>
2025-07-31 14:52:31 +0530

    chore: update class documentation

--- Examples/SwiftUIExample/SwiftUIExample/CustomPlugins/EventFilteringPlugin.swift
+++ Examples/SwiftUIExample/SwiftUIExample/CustomPlugins/EventFilteringPlugin.swift
@@ -8,17 +8,35 @@
 import Foundation
 import RudderStackAnalytics
 
+// MARK: - EventFilteringPlugin
+/**
+ This plugin filters out specific analytics events from being processed in the analytics pipeline. It allows you to prevent certain events from being tracked or sent to destinations.
+ 
+ ## Usage:
+ ```swift
+ // Create and add the plugin
+ let eventFilteringPlugin = EventFilteringPlugin()
+ analytics.add(plugin: eventFilteringPlugin)
+ ```
+*/
 final class EventFilteringPlugin: Plugin {
     var pluginType: PluginType = .onProcess
     var analytics: Analytics?
     
     private var eventsToFilter = [String]()
     
+    /** Called when the plugin is added to the analytics instance */
     func setup(analytics: Analytics) {
         self.analytics = analytics
         self.eventsToFilter = ["Application Opened", "Application Backgrounded"]
     }
     
+    /** 
+     Intercepts analytics events and filters out specified track events
+     
+     - Parameter event: The event to potentially filter
+     - Returns: The original event if it should be processed, or nil if it should be filtered out
+     */
     func intercept(event: any Event) -> (any Event)? {
         if let trackEvent = event as? TrackEvent, eventsToFilter.contains(trackEvent.event) {
             LoggerAnalytics.verbose(log: "EventFilteringPlugin: Event \"\(trackEvent.event)\" is filtered out.")
@@ -27,6 +45,7 @@ final class EventFilteringPlugin: Plugin {
         return event
     }
     
+    /** Called when the plugin is being removed or the analytics instance is being torn down */
     func teardown() {
         eventsToFilter.removeAll()
     }
