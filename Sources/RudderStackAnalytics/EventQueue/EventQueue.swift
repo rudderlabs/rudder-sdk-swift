30de0d819328b8991d92a7be38d74dca9e2702b2
Satheesh Kannan <satheesh@rudderstack.com>
2025-08-20 13:36:10 +0530

    refactor: move processing event class to separate file

--- Sources/RudderStackAnalytics/EventQueue/EventQueue.swift
+++ Sources/RudderStackAnalytics/EventQueue/EventQueue.swift
@@ -70,27 +70,3 @@ extension EventQueue {
         self.eventWriter?.stop()
     }
 }
-
-// MARK: - ProcessingEvent
-/**
- * ProcessingEvent represents an event in the processing pipeline.
- * It can be either a regular message event or a flush signal.
- */
-class ProcessingEvent {
-    var type: ProcessingEventType
-    var event: Event?
-    
-    init(type: ProcessingEventType, event: Event? = nil) {
-        self.type = type
-        self.event = event
-    }
-}
-
-// MARK: - ProcessingEventType
-/**
- * Enum representing the different types of processing events.
- */
-enum ProcessingEventType {
-    case message
-    case flush
-}
---
+++ Sources/RudderStackAnalytics/EventQueue/ProcessingEvent.swift
@@ -0,0 +1,32 @@
+//
+//  ProcessingEvent.swift
+//  RudderStackAnalyticsTests
+//
+//  Created by Satheesh Kannan on 20/08/25.
+//
+
+import Foundation
+
+// MARK: - ProcessingEventType
+/**
+ Enum representing the different types of processing events.
+ */
+enum ProcessingEventType {
+    case message
+    case flush
+}
+
+// MARK: - ProcessingEvent
+/**
+ ProcessingEvent represents an event in the processing pipeline.
+ It can be either a regular message event or a flush signal.
+ */
+class ProcessingEvent {
+    var type: ProcessingEventType
+    var event: Event?
+    
+    init(type: ProcessingEventType, event: Event? = nil) {
+        self.type = type
+        self.event = event
+    }
+}
