6e5738c5eead76dc41e75a777faaba36c8fe614c
Satheesh Kannan <satheesh@rudderstack.com>
2025-08-29 18:31:29 +0530

    feat: add BackoffPolicy protocol implementation

---
+++ Sources/RudderStackAnalytics/Policies/BackOffPolicy.swift
@@ -0,0 +1,44 @@
+//
+//  BackOffPolicy.swift
+//  RudderStackAnalytics
+//
+//  Created by Satheesh Kannan on 29/08/25.
+//
+
+import Foundation
+
+// MARK: - BackoffPolicy Protocol
+/**
+ Protocol representing a backoff policy for retrying operations.
+ Implementations should provide a strategy for calculating the next delay
+ and resetting the backoff state.
+ */
+protocol BackoffPolicy {
+    /**
+     Calculates the next delay in milliseconds based on the backoff policy.
+     
+     - Returns: The next delay in milliseconds.
+     */
+    func nextDelayInMilliseconds() -> Int
+    
+    /**
+     Resets the backoff policy to its initial state.
+     This method should be called when the backoff policy needs to be restarted.
+     */
+    func resetBackoff()
+}
+
+// MARK: - BackOffPolicyConstants
+
+/**
+ A struct containing constants used in backoff policies.
+ */
+struct BackOffPolicyConstants {
+
+    private init() {
+        /* Default implementation (no-op) */
+    }
+
+    static let minDelayInMillis = 3000
+    static let base = 2.0
+}
