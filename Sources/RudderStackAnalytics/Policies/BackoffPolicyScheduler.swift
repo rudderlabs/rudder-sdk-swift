267d3914e68db500526e86cb41ed9009904a3090
Satheesh Kannan <satheesh@rudderstack.com>
2025-08-29 20:15:17 +0530

    feat: add backoff policy scheduler class implementation

--- Sources/RudderStackAnalytics/Policies/BackoffPolicyScheduler.swift
+++ Sources/RudderStackAnalytics/Policies/BackoffPolicyScheduler.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+final actor BackoffPolicyScheduler {
+    private let maxAttempts: Int
+    private let coolOffSecs: Int
+    private var policy: BackoffPolicy
+    private var currentAttempt: Int
+    
+    init() {
+        self.maxAttempts = BackOffPolicyConstants.maxAttempts
+        self.coolOffSecs = BackOffPolicyConstants.coolOffPeriodInSeconds
+        self.policy = ExponentialBackoffPolicy()
+        self.currentAttempt = 0
+    }
+    
+    func waitWithBackoff() async {
+        currentAttempt += 1
+        
+        if currentAttempt > maxAttempts {
+            await applyCoolOffPeriod()
+        } else {
+            await applyBackoff()
+        }
+    }
+    
+    private func applyCoolOffPeriod() async {
+        LoggerAnalytics.verbose(log: "Max attempts reached. Entering cool-off period for upload queue")
+        self.reset()
+        LoggerAnalytics.verbose(log: "Next attempt will be after \(coolOffSecs) secs")
+        try? await self.sleep(seconds: coolOffSecs)
+    }
+    
+    private func applyBackoff() async {
+        let delay = policy.nextDelayInSeconds()
+        LoggerAnalytics.verbose(log: "Sleeping for \(delay) secs (attempt \(currentAttempt) of \(maxAttempts))")
+        try? await self.sleep(seconds: delay)
+    }
+    
+    func reset() {
+        LoggerAnalytics.verbose(log: "Resetting retry attempts and backoff policy")
+        currentAttempt = 0
+        policy.resetBackoff()
+    }
+}
+
+extension BackoffPolicyScheduler {
+    func sleep(seconds: Int) async throws {
+        try await Task.sleep(nanoseconds: UInt64(seconds) * 1_000_000_000)
+    }
+}
