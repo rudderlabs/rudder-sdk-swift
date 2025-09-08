1ae9ee7b142f779284271881ef0737366d216baa
Satheesh Kannan <satheesh@rudderstack.com>
2025-09-08 19:16:48 +0530

    chore: add reset options and reset entries class implementation

--- Sources/RudderStackAnalytics/StateManagement/ResetOptions.swift
+++ Sources/RudderStackAnalytics/StateManagement/ResetOptions.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+/**
+ Class representing options for resetting user data in the analytics SDK.
+ */
+public class ResetOptions {
+    public let entries: ResetEntries
+    
+    /**
+     Initializes a new instance of `ResetOptions`.
+     
+     - Parameters:
+       - entries: An instance of `ResetEntries` specifying which data to reset. Default is a new instance with all entries set to `true`.
+     */
+    public init(entries: ResetEntries = ResetEntries()) {
+        self.entries = entries
+    }
+}
+
+/**
+ Class representing specific entries to reset during a user data reset operation.
+ */
+public class ResetEntries {
+    public let anonymousId: Bool
+    public let userId: Bool
+    public let traits: Bool
+    public let session: Bool
+
+    /**
+     Initializes a new instance of `ResetEntries`.
+     
+     - Parameters:
+       - anonymousId: Whether to reset the anonymous ID. Default is `true`.
+       - userId: Whether to reset the user ID. Default is `true`.
+       - traits: Whether to reset user traits. Default is `true`.
+       - session: Whether to reset session information. Default is `true`.
+     */
+    public init(anonymousId: Bool = true, userId: Bool = true, traits: Bool = true, session: Bool = true) {
+        self.anonymousId = anonymousId
+        self.userId = userId
+        self.traits = traits    
+        self.session = session
+    }
+}
