92975c77e275646c4c2a8cd1c626bb0265361426
Vishal Gupta <vguptakota1998@gmail.com>
2025-10-27 19:14:39 +0530

    refactor: IntegrationsController for better readability

--- Sources/RudderStackAnalytics/Plugins/DeviceMode/IntegrationsController.swift
+++ Sources/RudderStackAnalytics/Plugins/DeviceMode/IntegrationsController.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+/**
+ This class is responsible for initializing or updating integrations based on the source configuration. It also manages the lifecycle of integrations by maintaining their references and invoking the appropriate `add`, `remove`, `reset`, and `flush` APIs as required.
+ */
 class IntegrationsController {
     
     var integrationPluginChain: PluginChain?
@@ -26,107 +29,6 @@ class IntegrationsController {
         }
         
         safelyInitOrUpdateAndNotify(destinationConfig: destinationConfig, integration: integration)
-    }
-    
-    private func isDestinationConfigured(sourceConfig: SourceConfig, integration: IntegrationPlugin) -> [String: Any]? {
-        guard let pluginStore = integration.pluginStore else { return nil }
-        
-        if !pluginStore.isStandardIntegration {
-            return [:]
-        }
-        
-        guard let destination = findDestination(sourceConfig: sourceConfig, key: integration.key) else {
-            let errorMessage = "Destination \(integration.key) not found in the source config. No events will be sent to this destination."
-            LoggerAnalytics.warn("IntegrationsController: \(errorMessage)")
-            safelyUpdateOnFailureAndNotify(
-                throwable: NSError(domain: "IntegrationsController", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage]),
-                integration: integration
-            )
-            return nil
-        }
-        
-        if !destination.isDestinationEnabled {
-            let errorMessage = "Destination \(integration.key) is disabled in dashboard. No events will be sent to this destination."
-            LoggerAnalytics.warn("IntegrationsController: \(errorMessage)")
-            safelyUpdateOnFailureAndNotify(
-                throwable: NSError(domain: "IntegrationsController", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage]),
-                integration: integration
-            )
-            return nil
-        }
-        
-        return destination.destinationConfig.rawDictionary
-    }
-    
-    private func safelyInitOrUpdateAndNotify(destinationConfig: [String: Any], integration: IntegrationPlugin) {
-        if integration.getDestinationInstance() == nil {
-            safelyCreateAndNotify(destinationConfig: destinationConfig, integration: integration)
-        } else {
-            safelyUpdateAndNotify(destinationConfig: destinationConfig, integration: integration)
-        }
-    }
-    
-    private func safelyCreateAndNotify(destinationConfig: [String: Any], integration: IntegrationPlugin) {
-        do {
-            try integration.create(destinationConfig: destinationConfig)
-            LoggerAnalytics.debug("IntegrationsController: Destination \(integration.key) created successfully.")
-            integration.pluginStore?.isDestinationReady = true
-            notifyCallbacks(.success, for: integration)
-        } catch {
-            LoggerAnalytics.error("IntegrationsController: Error: \(error.localizedDescription) creating destination \(integration.key).")
-            integration.pluginStore?.isDestinationReady = false
-            notifyCallbacks(.failure(error), for: integration)
-        }
-    }
-    
-    private func safelyUpdateOnFailureAndNotify(throwable: Error, integration: IntegrationPlugin) {
-        safelyUpdateAndApplyBlock(
-            destinationConfig: [:],
-            integration: integration,
-            block: {
-                LoggerAnalytics.debug("IntegrationsController: Destination \(integration.key) updated with empty destinationConfig.")
-                integration.pluginStore?.isDestinationReady = false
-                self.notifyCallbacks(.failure(throwable), for: integration)
-            }
-        )
-    }
-    
-    private func safelyUpdateAndNotify(destinationConfig: [String: Any], integration: IntegrationPlugin) {
-        safelyUpdateAndApplyBlock(
-            destinationConfig: destinationConfig,
-            integration: integration,
-            block: {
-                LoggerAnalytics.debug("IntegrationsController: Destination \(integration.key) updated with destinationConfig: \(destinationConfig).")
-                integration.pluginStore?.isDestinationReady = true
-                self.notifyCallbacks(.success, for: integration)
-            }
-        )
-    }
-    
-    private func safelyUpdateAndApplyBlock(destinationConfig: [String: Any], integration: IntegrationPlugin, block: @escaping () -> Void) {
-        guard let pluginStore = integration.pluginStore else { return }
-        
-        do {
-            // updating is only done for standard integrations as they depend on SourceConfig
-            if pluginStore.isStandardIntegration {
-                try integration.update(destinationConfig: destinationConfig)
-                block()
-            }
-        } catch {
-            LoggerAnalytics.error("IntegrationsController: Error: \(error.localizedDescription) updating destination \(integration.key).")
-            integration.pluginStore?.isDestinationReady = false
-            notifyCallbacks(.failure(error), for: integration)
-        }
-    }
-    
-    func notifyCallbacks(_ result: DestinationResult, for integration: IntegrationPlugin) {
-        guard let pluginStore = integration.pluginStore else { return }
-        // Atomically read and clear callbacks
-        pluginStore.$destinationReadyCallbacks.modify { callbacks in
-            let instance = integration.getDestinationInstance()
-            callbacks.forEach { $0(instance, result) }
-            callbacks.removeAll()
-        }
     }
     
     func add(integration: IntegrationPlugin) {
@@ -171,10 +73,6 @@ class IntegrationsController {
         }
     }
     
-    private func findDestination(sourceConfig: SourceConfig, key: String) -> Destination? {
-        return sourceConfig.source.destinations.first { $0.destinationDefinition.displayName == key }
-    }
-    
     deinit {
         $integrationPluginStores.modify { stores in
             stores.removeAll()
@@ -182,5 +80,113 @@ class IntegrationsController {
         self.integrationPluginChain?.removeAll()
         self.analytics = nil
         self.integrationPluginChain = nil
+    }
+}
+
+private extension IntegrationsController {
+    
+    func isDestinationConfigured(sourceConfig: SourceConfig, integration: IntegrationPlugin) -> [String: Any]? {
+        guard let pluginStore = integration.pluginStore else { return nil }
+        
+        if !pluginStore.isStandardIntegration {
+            return [:]
+        }
+        
+        guard let destination = findDestination(sourceConfig: sourceConfig, key: integration.key) else {
+            let errorMessage = "Destination \(integration.key) not found in the source config. No events will be sent to this destination."
+            LoggerAnalytics.warn("IntegrationsController: \(errorMessage)")
+            safelyUpdateOnFailureAndNotify(
+                throwable: NSError(domain: "IntegrationsController", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage]),
+                integration: integration
+            )
+            return nil
+        }
+        
+        if !destination.isDestinationEnabled {
+            let errorMessage = "Destination \(integration.key) is disabled in dashboard. No events will be sent to this destination."
+            LoggerAnalytics.warn("IntegrationsController: \(errorMessage)")
+            safelyUpdateOnFailureAndNotify(
+                throwable: NSError(domain: "IntegrationsController", code: -1, userInfo: [NSLocalizedDescriptionKey: errorMessage]),
+                integration: integration
+            )
+            return nil
+        }
+        
+        return destination.destinationConfig.rawDictionary
+    }
+    
+    func safelyInitOrUpdateAndNotify(destinationConfig: [String: Any], integration: IntegrationPlugin) {
+        if integration.getDestinationInstance() == nil {
+            safelyCreateAndNotify(destinationConfig: destinationConfig, integration: integration)
+        } else {
+            safelyUpdateAndNotify(destinationConfig: destinationConfig, integration: integration)
+        }
+    }
+    
+    func safelyCreateAndNotify(destinationConfig: [String: Any], integration: IntegrationPlugin) {
+        do {
+            try integration.create(destinationConfig: destinationConfig)
+            LoggerAnalytics.debug("IntegrationsController: Destination \(integration.key) created successfully.")
+            integration.pluginStore?.isDestinationReady = true
+            notifyCallbacks(.success, for: integration)
+        } catch {
+            LoggerAnalytics.error("IntegrationsController: Error: \(error.localizedDescription) creating destination \(integration.key).")
+            integration.pluginStore?.isDestinationReady = false
+            notifyCallbacks(.failure(error), for: integration)
+        }
+    }
+    
+    func safelyUpdateOnFailureAndNotify(throwable: Error, integration: IntegrationPlugin) {
+        safelyUpdateAndApplyBlock(
+            destinationConfig: [:],
+            integration: integration,
+            block: {
+                LoggerAnalytics.debug("IntegrationsController: Destination \(integration.key) updated with empty destinationConfig.")
+                integration.pluginStore?.isDestinationReady = false
+                self.notifyCallbacks(.failure(throwable), for: integration)
+            }
+        )
+    }
+    
+    func safelyUpdateAndNotify(destinationConfig: [String: Any], integration: IntegrationPlugin) {
+        safelyUpdateAndApplyBlock(
+            destinationConfig: destinationConfig,
+            integration: integration,
+            block: {
+                LoggerAnalytics.debug("IntegrationsController: Destination \(integration.key) updated with destinationConfig: \(destinationConfig).")
+                integration.pluginStore?.isDestinationReady = true
+                self.notifyCallbacks(.success, for: integration)
+            }
+        )
+    }
+    
+    func safelyUpdateAndApplyBlock(destinationConfig: [String: Any], integration: IntegrationPlugin, block: @escaping () -> Void) {
+        guard let pluginStore = integration.pluginStore else { return }
+        
+        do {
+            // updating is only done for standard integrations as they depend on SourceConfig
+            if pluginStore.isStandardIntegration {
+                try integration.update(destinationConfig: destinationConfig)
+                block()
+            }
+        } catch {
+            LoggerAnalytics.error("IntegrationsController: Error: \(error.localizedDescription) updating destination \(integration.key).")
+            integration.pluginStore?.isDestinationReady = false
+            notifyCallbacks(.failure(error), for: integration)
+        }
+    }
+    
+    func notifyCallbacks(_ result: DestinationResult, for integration: IntegrationPlugin) {
+        guard let pluginStore = integration.pluginStore else { return }
+        // Atomically read and clear callbacks
+        pluginStore.$destinationReadyCallbacks.modify { callbacks in
+            let instance = integration.getDestinationInstance()
+            callbacks.forEach { $0(instance, result) }
+            callbacks.removeAll()
+        }
+    }
+    
+    func findDestination(sourceConfig: SourceConfig, key: String) -> Destination? {
+        return sourceConfig.source.destinations.first { $0.destinationDefinition.displayName == key }
     }
 }
