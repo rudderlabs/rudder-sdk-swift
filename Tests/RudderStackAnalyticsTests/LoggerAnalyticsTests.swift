a531d1b9601ece86cba26597e187ea8a302cf432
Satheesh Kannan <satheesh@rudderstack.com>
2025-11-13 00:14:43 +0530

    chore: update LoggerAnalytics test cases

--- Tests/RudderStackAnalyticsTests/LoggerAnalyticsTests.swift
+++ Tests/RudderStackAnalyticsTests/LoggerAnalyticsTests.swift
@@ -5,112 +5,62 @@
 //  Created by Satheesh Kannan on 19/04/25.
 //
 
-import XCTest
+import Testing
+import Foundation
 @testable import RudderStackAnalytics
 
-final class LoggerAnalyticsTests: XCTestCase {
+@Suite("LoggerAnalytics Tests")
+struct LoggerAnalyticsTests {
     
-    var mockLogger: MockLogger?
+    var mockLogger: SwiftMockLogger
     
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        self.mockLogger = MockLogger()
+    init() {
+        mockLogger = SwiftMockLogger()
     }
     
-    override func tearDownWithError() throws {
-        try super.tearDownWithError()
-        self.mockLogger = nil
+    @Test("given a mock logger with info level, when calling each log method, then only info/warn/error messages are logged")
+    func loggerLogsAtCorrectLevels() {
+        LoggerAnalytics.setLogger(mockLogger)
+        LoggerAnalytics.logLevel = .info
+        
+        LoggerAnalytics.verbose("This is verbose")
+        LoggerAnalytics.debug("This is debug")
+        LoggerAnalytics.info("This is info")
+        LoggerAnalytics.warn("This is warn")
+        LoggerAnalytics.error("This is error")
+        
+        let loggedLevels = mockLogger.logs.map { $0.level }
+        #expect(!loggedLevels.contains("VERBOSE"))
+        #expect(!loggedLevels.contains("DEBUG"))
+        #expect(loggedLevels.contains("INFO"))
+        #expect(loggedLevels.contains("WARN"))
+        #expect(loggedLevels.contains("ERROR"))
     }
     
-    func testLoggerLogsAtCorrectLevels() {
-        given("a mock logger and log level set to .info") {
-            guard let mockLogger else { XCTFail("Mock logger not set up correctly"); return }
-            LoggerAnalytics.setLogger(mockLogger)
-            LoggerAnalytics.logLevel = .info
-            
-            when("calling each log method") {
-                LoggerAnalytics.verbose("This is verbose")
-                LoggerAnalytics.debug("This is debug")
-                LoggerAnalytics.info("This is info")
-                LoggerAnalytics.warn("This is warn")
-                LoggerAnalytics.error("This is error")
-                
-                then("only info, warn, and error messages should be logged") {
-                    let loggedLevels = mockLogger.logs.map { $0.level }
-                    XCTAssertFalse(loggedLevels.contains("VERBOSE"))
-                    XCTAssertFalse(loggedLevels.contains("DEBUG"))
-                    XCTAssertTrue(loggedLevels.contains("INFO"))
-                    XCTAssertTrue(loggedLevels.contains("WARN"))
-                    XCTAssertTrue(loggedLevels.contains("ERROR"))
-                }
-            }
-        }
+    @Test("given a mock logger with error level, when logging error with and without error object, then both errors are logged with correct messages")
+    func errorLoggingWithAndWithoutErrorObject() {
+        LoggerAnalytics.setLogger(mockLogger)
+        LoggerAnalytics.logLevel = .error
+        
+        LoggerAnalytics.error("Only log")
+        LoggerAnalytics.error("With error", cause: NSError(domain: "Test", code: 1))
+        
+        #expect(mockLogger.logs.count == 2)
+        #expect(mockLogger.logs[0].message.contains("Only log"))
+        #expect(mockLogger.logs[1].message.contains("With error"))
     }
     
-    func testErrorLoggingWithAndWithoutErrorObject() {
-        given("a mock logger and log level set to .error") {
-            guard let mockLogger else { XCTFail("Mock logger not set up correctly"); return }
-            LoggerAnalytics.setLogger(mockLogger)
-            LoggerAnalytics.logLevel = .error
-            
-            when("calling error with and without an error object") {
-                LoggerAnalytics.error("Only log")
-                LoggerAnalytics.error("With error", cause: NSError(domain: "Test", code: 1))
-                
-                then("both error messages should be captured") {
-                    XCTAssertEqual(mockLogger.logs.count, 2)
-                    XCTAssertTrue(mockLogger.logs[0].message.contains("Only log"))
-                    XCTAssertTrue(mockLogger.logs[1].message.contains("With error"))
-                }
-            }
-        }
-    }
-    
-    func testNoLoggingWhenLevelIsNone() {
-        given("a mock logger and log level set to .none") {
-            guard let mockLogger else { XCTFail("Mock logger not set up correctly"); return }
-            LoggerAnalytics.setLogger(mockLogger)
-            LoggerAnalytics.logLevel = .none
-            
-            when("all log methods are called") {
-                LoggerAnalytics.verbose("V")
-                LoggerAnalytics.debug("D")
-                LoggerAnalytics.info("I")
-                LoggerAnalytics.warn("W")
-                LoggerAnalytics.error("E")
-                
-                then("no messages should be logged") {
-                    XCTAssertTrue(mockLogger.logs.isEmpty)
-                }
-            }
-        }
-    }
-}
-
-class MockLogger: Logger {
-    var logs: [(level: String, message: String)] = []
-    
-    func verbose(log: String) {
-        logs.append(("VERBOSE", log))
-    }
-    
-    func debug(log: String) {
-        logs.append(("DEBUG", log))
-    }
-    
-    func info(log: String) {
-        logs.append(("INFO", log))
-    }
-    
-    func warn(log: String) {
-        logs.append(("WARN", log))
-    }
-    
-    func error(log: String, error: Error?) {
-        if let error {
-            logs.append(("ERROR", "\(log) - \(error.localizedDescription)"))
-        } else {
-            logs.append(("ERROR", log))
-        }
+    @Test("given a mock logger with none level, when calling all log methods, then no logs are captured")
+    func noLoggingWhenLevelIsNone() {
+        LoggerAnalytics.setLogger(mockLogger)
+        LoggerAnalytics.logLevel = .none
+        
+        LoggerAnalytics.verbose("This is verbose")
+        LoggerAnalytics.debug("This is debug")
+        LoggerAnalytics.info("This is info")
+        LoggerAnalytics.warn("This is warn")
+        LoggerAnalytics.error("This is error")
+        
+        #expect(mockLogger.logs.isEmpty)
     }
 }
