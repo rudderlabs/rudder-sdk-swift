11964bb15101aae5fd3d8ce24f61df045770bdd9
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-12-16 10:59:45 +0530

    chore: replace decoder with an runtime exception
    As this code is never used internally.
    
    🔒 Scanned for secrets using gitleaks 8.28.0

--- Sources/RudderStackAnalytics/Helpers/CoreSupport/CodableTypes.swift
+++ Sources/RudderStackAnalytics/Helpers/CoreSupport/CodableTypes.swift
@@ -90,34 +90,16 @@ public struct AnyCodable: Codable {
         self.value = value
     }
     
+    /**
+     Decoding initializer required by the Codable protocol.
+
+     AnyCodable is designed for encoding only. Use `init(_ value: Any)` to create instances.
+     This method will always throw a `DecodingError` if invoked.
+     */
     public init(from decoder: Decoder) throws {
+        LoggerAnalytics.error("AnyCodable decoding is not supported")
         let container = try decoder.singleValueContainer()
-        
-        if let intValue = try? container.decode(Int.self) {
-            value = intValue
-        } else if let intValue = try? container.decode(UInt64.self) {
-            value = intValue
-        } else if let doubleValue = try? container.decode(Double.self) {
-            value = doubleValue
-        } else if let cgfloatValue = try? container.decode(CGFloat.self) {
-            value = cgfloatValue
-        } else if let boolValue = try? container.decode(Bool.self) {
-            value = boolValue
-        } else if let stringValue = try? container.decode(String.self) {
-            if let url = URL(string: stringValue), stringValue.contains("://") || stringValue.contains("www.") {
-                value = url
-            } else if let date = Date.date(from: stringValue) {
-                value = date
-            } else {
-                value = stringValue
-            }
-        } else if let arrayValue = try? container.decode([AnyCodable].self) {
-            value = arrayValue.map { $0.value }
-        } else if let dictionaryValue = try? container.decode([String: AnyCodable].self) {
-            value = dictionaryValue.mapValues { $0.value }
-        } else {
-            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unsupported type")
-        }
+        throw DecodingError.dataCorruptedError(in: container, debugDescription: "AnyCodable decoding is not supported")
     }
 
     // swiftlint:disable:next cyclomatic_complexity
