12ae98f64be043a26d65073dfe15ffe97fa03d81
Satheesh Kannan <satheesh@rudderstack.com>
2025-12-23 11:25:36 +0530

    chore: update example app to use persistence migration

--- Examples/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
+++ Examples/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -33,6 +33,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
+        // Restore persistence from legacy storage if exists.
+        PersistenceMigrator(writeKey: "swift-sdk-write-key").restorePersistence()
+        
         self.permissionManager.requestPermissions([.idfa, .pushNotification, .bluetooth]) {
             print("All required permissions requested..")
             self.permissionsRequested = true
