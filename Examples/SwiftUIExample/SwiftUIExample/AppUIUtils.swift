0b2cab4b0d952d4ff3c3830ffddc306f789c3862
Satheesh Kannan <satheesh@rudderstack.com>
2026-04-01 22:55:38 +0530

    feat: add ActionButton and SectionHeader components

--- Examples/SwiftUIExample/SwiftUIExample/AppUIUtils.swift
+++ Examples/SwiftUIExample/SwiftUIExample/AppUIUtils.swift
@@ -7,6 +7,34 @@
 
 import SwiftUI
 
-extension Color {
-    static let rudderBlue = Color("RudderBlue")
+// MARK: - ActionButton
+
+struct ActionButton: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.system(size: 14, weight: .bold))
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 14)
+        }
+        .background(Color.rudderBlue)
+        .cornerRadius(12)
+    }
+}
+
+// MARK: - SectionHeader
+
+struct SectionHeader: View {
+    let title: String
+
+    var body: some View {
+        Text(title)
+            .font(.system(size: 14, weight: .bold))
+            .foregroundColor(.primary)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
 }
