fc53abf46256e311a22ed2007abffc94492e0b66
Satheesh Kannan <satheesh@rudderstack.com>
2026-04-01 22:45:37 +0530

    feat: add AppUIUtils for UI components and helpers

---
+++ Examples/SwiftUIExample/SwiftUIExample/AppUIUtils.swift
@@ -0,0 +1,12 @@
+//
+//  AppUIUtils.swift
+//  SwiftUIExampleApp
+//
+//  Created by Satheesh Kannan on 01/04/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let rudderBlue = Color("RudderBlue")
+}
