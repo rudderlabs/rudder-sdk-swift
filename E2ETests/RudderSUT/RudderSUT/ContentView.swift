caab2d4e67cb9735265f978a46bf5bdd64fd69d7
Satheesh Kannan <satheesh@rudderstack.com>
2026-05-07 14:05:20 +0530

    feat(e2e): scaffold RudderSUT app and RudderUITests XCUITest targets

---
+++ E2ETests/RudderSUT/RudderSUT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RudderSUT
+//
+//  Created by Satheesh Kannan on 07/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ E2ETests/RudderSUT/RudderSUT/RudderSUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RudderSUTApp.swift
+//  RudderSUT
+//
+//  Created by Satheesh Kannan on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RudderSUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
