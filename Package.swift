3b7f61c2c7fa7efd013cb1d135ce6c09b9fc60b9
Chryssa Aliferi <c.aliferi@gmail.com>
2026-07-16 18:27:21 +0300

    chore: relicense under MIT
    Replaces Elastic License 2.0 with the MIT License.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// License: Elastic License 2.0 (ELv2) – see LICENSE file for details
+// License: MIT – see LICENSE file for details
 
 import PackageDescription
 
