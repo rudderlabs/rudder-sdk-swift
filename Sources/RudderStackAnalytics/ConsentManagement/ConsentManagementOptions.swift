195461e33aa0142f00d31bbc4496d2a63b3b56f7
Satheesh Kannan <satheesh@rudderstack.com>
2026-08-13 13:18:24 +0530

    refactor: rename consent types per LLD review

--- Sources/RudderStackAnalytics/ConsentManagement/ConsentManagementOptions.swift
+++ Sources/RudderStackAnalytics/ConsentManagement/ConsentManagementOptions.swift
@@ -1,5 +1,5 @@
 //
-//  ConsentManagementUpdateOptions.swift
+//  ConsentManagementOptions.swift
 //  RudderStackAnalytics
 //
 //  Created by Satheesh Kannan on 13/08/26.
@@ -7,15 +7,15 @@
 
 import Foundation
 
-// MARK: - ConsentManagementUpdateOptions
+// MARK: - ConsentManagementOptions
 /**
  Options for updating consent at runtime via `Analytics.setConsent(_:)`.
 
  The supplied lists fully replace the current consent state. Omitted lists default
  to empty, which clears the corresponding values.
  */
-@objc(RSSConsentManagementUpdateOptions)
-public class ConsentManagementUpdateOptions: NSObject {
+@objc(RSSConsentManagementOptions)
+public class ConsentManagementOptions: NSObject {
     /**
      Consent category IDs the user has granted.
      */
